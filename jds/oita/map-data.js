3973d26d1c8ecc56357c4c5d2378b6f4def9a16b
sansymeon <stephenjnelson@gmail.com>
2025-12-10 12:48:16 +0900

    Create map-data.js

---
+++ jds/oita/map-data.js
@@ -0,0 +1,31 @@
+window.stations = [
+  { name: "大分駅", lat: 33.23232801877058, lng: 131.6057111319828 },
+  { name: "西大分駅", lat: 33.24522482617123, lng: 131.58285137901504 },
+  { name: "東別府駅", lat: 33.2677076187008, lng: 131.51050697420135 }
+];
+window.bookstores = [
+  { name: "TSUTAYA Mitsuyoshiten",  lat: 33.199082,   lng: 131.605068, }
+];
+window.coffeeshops = [
+  { name: "Starbucks Omichi Oita Bypass", lat: 33.212474, lng: 131.596578 },
+  { name: "Starbucks Nakakasuga Oita", lat: 33.23985440134827, lng: 131.59493664576107 }
+];
+window.artisans = [
+  { name: "Oita Made Shop アカレンガ", lat:　33.237182, lng: 131.60759945195588 },
+];
+window.noodles = [
+  { name: "Ramen Workshop Fukuya", lat: 33.2325999418626, lng: 131.60667938756507 }
+];
+window.restaurants = [
+  { name: "Konas Coffee Minami Oita", lat: 33.208655, lng: 131.599828 },
+131.5998847899164  
+];
+window.supermarkets = [
+  { name: "Melon Supermaket", lat: 33.233223, , lng: 131.598352 }
+];
+window.clinics = [
+  { name: "Hirano 内科", lat: , lng:  }
+];
+window.churches = [
+  { name: "St. Francis Xavier Cathedral", lat: 33.238665, lng: 131.604509 }
+];
