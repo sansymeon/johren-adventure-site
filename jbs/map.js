937fd9aa203c49d0761afdf7d0123c1cce86c728
sansymeon <stephenjnelson@gmail.com>
2025-12-13 16:14:44 +0900

    Update map.js

--- jbs/map.js
+++ jbs/map.js
@@ -12,7 +12,7 @@ const map = L.map('map', {
 // TILE LAYER
 // -------------------------------
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 8.2,
+  maxZoom: 8,
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
