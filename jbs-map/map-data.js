a08f957f43bb65dc9445d8f1c8bd1e65af264f6d
sansymeon <stephenjnelson@gmail.com>
2025-12-15 21:22:46 +0900

    Update map-data.js

--- jbs-map/map-data.js
+++ jbs-map/map-data.js
@@ -1,3 +1,8 @@
 window.samples = [
-  { name: "Johren サンプル・スポット", lat: 33.55857973584153,   lng:  130.21385183358143 },
+  {
+    name: "Johren サンプル・スポット",
+    lat: 33.55857973584153,
+    lng: 130.21385183358143,
+    url: "/merchant/sample/index.html"
+  }
 ];
