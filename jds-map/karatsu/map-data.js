e4ac6614ef74ae809bb661f2657df3b8ba144788
sansymeon <stephenjnelson@gmail.com>
2025-12-20 21:14:24 +0900

    Update map-data.js

--- jds-map/karatsu/map-data.js
+++ jds-map/karatsu/map-data.js
@@ -1,1 +1,20 @@
-
+window.churches = [
+  { name: "Karatsu Catholic Church", lat: 33.45252801155086,   lng: 129.9614152412968 }
+];
+window.museums = [
+  { name: "Hikiyama Exhibition Hall", lat: 33.445946414612074,  lng: 129.96991124129468 },
+  { name: "Karatsu Castle", lat: 33.45345765716856,   lng: 129.9782080418245 },
+  { name: "Former Takatori Residence 旧高取邸", lat: 33.45437629769318,  lng: 129.97209266442803 }
+];
+window.shrines = [
+  { name: "Karatsu Shrine　唐津神社", lat: 33.45227410257489,  lng: 129.96948404957564 },
+  { name: "Myoken Shrine　妙見神社", lat: 33.46854559781762,  lng: 129.95317852240166 }
+];
+window.stations = [
+  { name: "Hamasaki 浜崎駅", lat: 33.446720, lng: 130.036798 },
+  { name: "Watada 和多田駅", lat: 33.436933, lng: 129.980768 },
+  { name: "Karatsu 唐津駅", lat: 33.446223, lng: 129.967587 },
+];
+window.temples = [
+  { name: "Gonshoji Temple 近松寺", lat: 33.449901908390125,   lng: 129.96520797775324 }
+];
