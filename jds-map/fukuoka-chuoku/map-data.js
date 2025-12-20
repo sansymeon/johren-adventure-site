66c2c9b950a2e763d80a16ecf771bef711fe01c3
sansymeon <stephenjnelson@gmail.com>
2025-12-20 21:32:14 +0900

    Create map-data.js

---
+++ jds-map/fukuoka-chuoku/map-data.js
@@ -0,0 +1,30 @@
+window.churches = [
+  { name: "Daimyo Catholic Church", lat: 33.59028689597394,  lng: 130.39490595063134 },
+window.museums = [
+  { name: "Fukuoka Art Museum", lat: 33.58411652460625,   lng: 130.37958344698214 },
+];
+window.shrines = [
+  { name: "Terumo Shrine 光雲神社", lat: 33.597145537533365, lng: 130.37643231206792 },
+  { name: "Fukuoka Prefecture Gokoku Shrine 福岡縣護國神社", lat: 33.581715227750, lng: 130.38203472165 }
+];
+window.stations = [
+  { name: "Airpot 福岡空港駅", lat: 33.59719916364836,  lng: 130.44822439462428  },
+  { name: "Hakata 博多駅", lat: 33.589580007114,  lng: 130.42049136024366 },
+  { name: "Tenjin 天神駅", lat: 33.5913469503287,   lng: 130.39891098113026 },
+  { name: "Akasaka 赤坂駅",  lat: 33.58906127661953,   lng: 130.3906207746017 },
+  { name: "Ohori Park 大濠公園駅", lat: 33.59012812434707,  lng: 130.3788320713684 },
+  { name: "Tohjinmachi 唐人町駅", lat: 33.590145613562676,  lng: 130.36996167573926 },
+  { name: "Nishijin 西新駅", lat: 33.583574101225004, lng: 130.3592916253071  },
+  { name: "Fujisaki 藤崎駅", lat: 33.581378487964386,   lng: 130.34798479092692 },
+  { name: "Meinohama 姪浜", lat: 33.583729590361415,   lng: 130.3249049548341  },
+
+  { name: "Kaizuka 貝塚駅", lat: 33.63193553648232,  lng: 130.4256637939082　},
+
+  { name: "Kushida Shrine 櫛田神社駅", lat: 33.59162094482371,   lng: 130.41101054675016 },
+  { name: "Tenjin-minami 天神南駅", lat: 33.5889043128091,   lng: 130.40256933922515 },
+  { name: "Ropponmatsu 六本松駅", lat: 33.577600387437386,    lng: 130.37719613325575 },
+
+  { name: "Hashimoto 橋本駅", lat: 33.55532777061,    lng: 130.32088013037475 },
+
+  { name: "Takamiya 高宮駅", lat: 33.567008496728086,    lng:  130.41462886255　},
+];
