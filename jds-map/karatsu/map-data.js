7be8d58d282a8b69162e280c57575d234a4a7d24
sansymeon <stephenjnelson@gmail.com>
2025-12-22 09:34:55 +0900

    Update map-data.js

--- jds-map/karatsu/map-data.js
+++ jds-map/karatsu/map-data.js
@@ -11,9 +11,23 @@ window.shrines = [
   { name: "Myoken Shrine　妙見神社", lat: 33.46854559781762,  lng: 129.95317852240166 }
 ];
 window.stations = [
+  { name: "Chikuzen Maebaru 筑前前原駅", lat: 33.557082, lng: 130.199305 },
+  { name: "Misakigaoka 美咲が丘駅", lat: 33.549959, lng: 130.185515 },
+  { name: "Kafuri 加布里駅", lat: 33.543150, lng: 130.176150 },
+  { name: "Ikisan 一貴山駅", lat: 33.534295, lng: 130.168242 },
+  { name: "Chikuzen Fukae 筑前深江駅", lat: 33.514949, lng: 130.139429 },
+  { name: "Dainyu 大入駅", lat: 33.508855, lng: 130.106038 },
+  { name: "Fukuyoshi 福吉駅", lat: 33.501554, lng: 130.079523 },
+  { name: "Shika 鹿家駅", lat: 33.484259, lng: 130.047476 },
   { name: "Hamasaki 浜崎駅", lat: 33.446720, lng: 130.036798 },
+  { name: "Nijino Matsubara 虹の松原駅", lat: 33.441033, lng: 130.016195 },
+  { name: "Higashi Karatsu 東唐津駅", lat: 33.439393, lng: 129.996837 },
   { name: "Watada 和多田駅", lat: 33.436933, lng: 129.980768 },
   { name: "Karatsu 唐津駅", lat: 33.446223, lng: 129.967587 },
+  { name: "Nishi Karatsu 西唐津駅", lat: 33.461434, lng: 129.957480 },
+  { name: "Yamamoto 山本駅", lat: 33.39278208271683,  lng: 129.98118155167737 },
+  { name: "Ouchi 相知駅", lat: 33.34699039849446, lng: 130.01952634118035  },
+  { name: "Iwaya 岩屋駅", lat: 33.33076505677805,  lng: 130.03827683163834 }
 ];
 window.temples = [
   { name: "Gonshoji Temple 近松寺", lat: 33.449901908390125,   lng: 129.96520797775324 }
