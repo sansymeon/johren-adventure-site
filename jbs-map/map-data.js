6b9e920747ede12c496c3b52059919ec699795e1
sansymeon <stephenjnelson@gmail.com>
2025-12-22 16:47:52 +0900

    Update map-data.js

--- jbs-map/map-data.js
+++ jbs-map/map-data.js
@@ -3,12 +3,10 @@
 // Others are pre-registered but unpublished.
 
 window.samples = [
-  {
-    name: "Johren サンプル・スポット",
-    lat: 33.55857973584153,
-    lng: 130.21385183358143,
-    url: "/merchant/sample/index.html"
-  }
+  { name: "Johren Itoshima サンプル・スポット", lat: 33.55857973584153, lng: 130.21385183358143, url: "/merchant/sample/index.html" },
+  { name: "Johren Karatsu サンプル・スポット", lat: 33.44973729276236,  lng: 129.96801192244578, url: "/merchant/sample/index.html" },
+  { name: "Johren Fukuoka-Chuo サンプル・スポット", lat: 33.58620293814177, lng: 130.3764331173192, url: "/merchant/sample/index.html" },
+  { name: "Johren Oita サンプル・スポット", lat: 33.23137680796785,  lng: 131.60516398591903, url: "/merchant/sample/index.html" },
 ];
 
 window.artisans = [
