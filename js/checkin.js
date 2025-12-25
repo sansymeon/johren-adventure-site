ec35a1aec9d47a69309be5333913899af2818ff1
sansymeon <stephenjnelson@gmail.com>
2025-12-25 10:53:46 +0900

    Update checkin.js

--- js/checkin.js
+++ js/checkin.js
@@ -1,1 +1,7 @@
+const params = new URLSearchParams(window.location.search);
+const stationId = params.get('id');
+
+if (stationId && typeof markStationVisited === 'function') {
+  markStationVisited(stationId);
+}
 
