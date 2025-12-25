d5444605abff5bf8f46de9b38ec0bd0064998db8
sansymeon <stephenjnelson@gmail.com>
2025-12-25 10:50:31 +0900

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,21 @@
+const STORAGE_KEY = 'johren';
+
+function getJohrenData() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+    visitedStations: []
+  };
+}
+
+function saveJohrenData(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function markStationVisited(stationId) {
+  const data = getJohrenData();
+
+  if (!data.visitedStations.includes(stationId)) {
+    data.visitedStations.push(stationId);
+    saveJohrenData(data);
+  }
+}
 
