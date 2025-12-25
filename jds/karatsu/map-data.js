2b7e902d684dfd0a51a86c37df4f656f3821cee0
sansymeon <stephenjnelson@gmail.com>
2025-12-25 22:29:42 +0900

    Update map-data.js

--- jds/karatsu/map-data.js
+++ jds/karatsu/map-data.js
@@ -1,41 +1,47 @@
 window.MAP_CONFIG = {
   areaKey: 'karatsu',
-  center: [33.xxx, 129.xxx],
-  zoom: 12,
-  stations: [...]
-};
 
-window.churches = [
-  { name: "Karatsu Catholic Church", lat: 33.45252801155086,   lng: 129.9614152412968 }
-];
-window.museums = [
-  { name: "Hikiyama Exhibition Hall", lat: 33.445946414612074,  lng: 129.96991124129468 },
-  { name: "Karatsu Castle", lat: 33.45345765716856,   lng: 129.9782080418245 },
-  { name: "Former Takatori Residence 旧高取邸", lat: 33.45437629769318,  lng: 129.97209266442803 }
-];
-window.shrines = [
-  { name: "Karatsu Shrine　唐津神社", lat: 33.45227410257489,  lng: 129.96948404957564 },
-  { name: "Myoken Shrine　妙見神社", lat: 33.46854559781762,  lng: 129.95317852240166 }
-];
-window.stations = [
-  { name: "Chikuzen Maebaru 筑前前原駅", lat: 33.557082, lng: 130.199305 },
-  { name: "Misakigaoka 美咲が丘駅", lat: 33.549959, lng: 130.185515 },
-  { name: "Kafuri 加布里駅", lat: 33.543150, lng: 130.176150 },
-  { name: "Ikisan 一貴山駅", lat: 33.534295, lng: 130.168242 },
-  { name: "Chikuzen Fukae 筑前深江駅", lat: 33.514949, lng: 130.139429 },
-  { name: "Dainyu 大入駅", lat: 33.508855, lng: 130.106038 },
-  { name: "Fukuyoshi 福吉駅", lat: 33.501554, lng: 130.079523 },
-  { name: "Shika 鹿家駅", lat: 33.484259, lng: 130.047476 },
-  { name: "Hamasaki 浜崎駅", lat: 33.446720, lng: 130.036798 },
-  { name: "Nijino Matsubara 虹の松原駅", lat: 33.441033, lng: 130.016195 },
-  { name: "Higashi Karatsu 東唐津駅", lat: 33.439393, lng: 129.996837 },
-  { name: "Watada 和多田駅", lat: 33.436933, lng: 129.980768 },
-  { name: "Karatsu 唐津駅", lat: 33.446223, lng: 129.967587 },
-  { name: "Nishi Karatsu 西唐津駅", lat: 33.461434, lng: 129.957480 },
-  { name: "Yamamoto 山本駅", lat: 33.39278208271683,  lng: 129.98118155167737 },
-  { name: "Ouchi 相知駅", lat: 33.34699039849446, lng: 130.01952634118035  },
-  { name: "Iwaya 岩屋駅", lat: 33.33076505677805,  lng: 130.03827683163834 }
-];
-window.temples = [
-  { name: "Gonshoji Temple 近松寺", lat: 33.449901908390125,   lng: 129.96520797775324 }
-];
+  center: [33.4533, 129.9782], // Karatsu Castle area
+  zoom: 12,
+
+  stations: [
+    { id: 'maebaru', name: '筑前前原駅', nameEn: 'Chikuzen Maebaru', lat: 33.557082, lng: 130.199305 },
+    { id: 'misakigaoka', name: '美咲が丘駅', nameEn: 'Misakigaoka', lat: 33.549959, lng: 130.185515 },
+    { id: 'kafuri', name: '加布里駅', nameEn: 'Kafuri', lat: 33.543150, lng: 130.176150 },
+    { id: 'ikisan', name: '一貴山駅', nameEn: 'Ikisan', lat: 33.534295, lng: 130.168242 },
+    { id: 'fukae', name: '筑前深江駅', nameEn: 'Chikuzen Fukae', lat: 33.514949, lng: 130.139429 },
+    { id: 'dainyu', name: '大入駅', nameEn: 'Dainyu', lat: 33.508855, lng: 130.106038 },
+    { id: 'fukuyoshi', name: '福吉駅', nameEn: 'Fukuyoshi', lat: 33.501554, lng: 130.079523 },
+    { id: 'shika', name: '鹿家駅', nameEn: 'Shika', lat: 33.484259, lng: 130.047476 },
+    { id: 'hamasaki', name: '浜崎駅', nameEn: 'Hamasaki', lat: 33.446720, lng: 130.036798 },
+    { id: 'nijinomatsubara', name: '虹の松原駅', nameEn: 'Nijinomatsubara', lat: 33.441033, lng: 130.016195 },
+    { id: 'higashikaratsu', name: '東唐津駅', nameEn: 'Higashi Karatsu', lat: 33.439393, lng: 129.996837 },
+    { id: 'watada', name: '和多田駅', nameEn: 'Watada', lat: 33.436933, lng: 129.980768 },
+    { id: 'karatsu', name: '唐津駅', nameEn: 'Karatsu', lat: 33.446223, lng: 129.967587 },
+    { id: 'nishikaratsu', name: '西唐津駅', nameEn: 'Nishi Karatsu', lat: 33.461434, lng: 129.957480 },
+    { id: 'yamamoto', name: '山本駅', nameEn: 'Yamamoto', lat: 33.39278208271683, lng: 129.98118155167737 },
+    { id: 'ouchi', name: '相知駅', nameEn: 'Ouchi', lat: 33.34699039849446, lng: 130.01952634118035 },
+    { id: 'iwaya', name: '岩屋駅', nameEn: 'Iwaya', lat: 33.33076505677805, lng: 130.03827683163834 }
+  ],
+
+  churches: [
+    { name: 'Karatsu Catholic Church', lat: 33.45252801155086, lng: 129.9614152412968 }
+  ],
+
+  museums: [
+    { name: 'Hikiyama Exhibition Hall', lat: 33.445946414612074, lng: 129.96991124129468 },
+    { name: 'Karatsu Castle', lat: 33.45345765716856, lng: 129.9782080418245 },
+    { name: 'Former Takatori Residence 旧高取邸', lat: 33.45437629769318, lng: 129.97209266442803 }
+  ],
+
+  shrines: [
+    { name: 'Karatsu Shrine 唐津神社', lat: 33.45227410257489, lng: 129.96948404957564 },
+    { name: 'Myoken Shrine 妙見神社', lat: 33.46854559781762, lng: 129.95317852240166 }
+  ],
+
+  temples: [
+    { name: 'Gonshoji Temple 近松寺', lat: 33.449901908390125, lng: 129.96520797775324 }
+  ],
+
+  parks: []
+};
