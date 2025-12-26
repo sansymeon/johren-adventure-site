e0d3d397c9a7a5305a43b97a1b93b3c3df057278
sansymeon <stephenjnelson@gmail.com>
2025-12-26 18:33:32 +0900

    Create map.data.original.js

---
+++ jds/itoshima/map.data.original.js
@@ -0,0 +1,195 @@
+window.MAP_CONFIG = {
+  itoshima: {
+    center: [33.557, 130.199],
+    zoom: 12,
+
+// JDS = community discovery map
+// No businesses, no permissions, no monetization logic
+
+ churches: [
+      { name: "Itoshima Catholic Church", lat: 33.584095, lng: 130.189016 },
+      { name: "Baptist Church 福岡西部バプテスト教会", lat: 33.56349797391542, lng: 130.20519427279035 },
+      { name: "West Fukuoka Hope Christian Church 西福岡のぞみキリスト教会", lat: 33.548273234052175, lng: 130.2120380977682 },
+      { name: "Maebaru Christian Church 日本基督教団 前原教会", lat: 33.55976434044294, lng: 130.19648464789108 },
+      { name: "LDS Church 末日聖徒イエス・キリスト教会 糸島ワード", lat: 33.564099560006774, lng: 130.20782879949556 
+      }
+    ],
+museums = [
+  { name: "Ito Art Museum", 
+   lat: 33.56063278048929,   
+   lng: 130.20447283810256 
+  },
+  { 
+    name: "Ito-koku History Museum", 
+   lat: 33.53641274192818,   
+   lng: 130.25185578816215 
+  }
+],
+shrines = [
+  { name: "Futamigaura 二見ヶ浦 海中大鳥居", lat: 33.6393416472,  lng: 130.1971275794},
+  { name: "Sakurai Shrine　櫻井神社", lat: 33.6281710061135, lng: 130.191945877441 
+  }
+],
+stations = [
+  { 
+    id: "jr-chikuhi-JK01", 
+    name: "姪浜", 
+    nameEn: "Meinohama", 
+    lat: 33.58370, 
+    lng: 130.32501 
+  },
+    { 
+    id: "jr-chikuhi-JK02", 
+      name: "下山門", 
+      nameEn: "Shimoyamato",  
+      lat: 33.581882, 
+      lng: 130.308038 
+    },
+  { 
+    id: "jr-chikuhi-JK03", 
+    name: "今宿", 
+    nameEn: "Imajuku",  
+    lat: 33.579257, 
+    lng: 130.273519 
+  },
+  { 
+    id: "jr-chikuhi-JK04", 
+   name: "九大学研都市", 
+    nameEn: "Kyudai Gakken",  
+    lat: 33.578150, 
+    lng: 130.259861 
+  },
+  { 
+    id: "jr-chikuhi-JK05", 
+   name: "周船寺", 
+   nameEn: "Susenji",  
+   lat: 33.570792, 
+   lng: 130.246136 
+  },
+  { 
+    id: "jr-chikuhi-JK06", 
+    name: "波多江", 
+    nameEn: "Hatae",  
+    lat: 33.563944, 
+    lng: 130.226822 
+  },
+  { 
+    id: "jr-chikuhi-JK07", 
+   name: "糸島高校前", 
+   nameEn: "Itoshimakoko-mae",  
+   lat: 33.560660, 
+   lng: 130.213243 
+  },
+  { 
+    id: "jr-chikuhi-JK08", 
+   name: "筑前前原", 
+   nameEn: "Maebaru",  
+   lat: 33.557082, 
+   lng: 130.199305 
+  },
+  { 
+    id: "jr-chikuhi-JK09", 
+   name: "美咲が丘", 
+   nameEn: "Misakigaoka",  
+   lat: 33.549959, 
+   lng: 130.185515 
+  },
+  { 
+    id: "jr-chikuhi-JK010", 
+   name: "加布里", 
+   nameEn: "Kafuri",  
+   lat: 33.543150, 
+   lng: 130.176150 
+  },
+  { 
+    id: "jr-chikuhi-JK011", 
+   name: "一貴山", 
+   nameEn: "Ikisan",  
+    lat: 33.534295, 
+    lng: 130.168242 
+  },
+  { 
+    id: "jr-chikuhi-JK012", 
+   name: "筑前深江", 
+   nameEn: "Fukae", 
+    lat: 33.514949, 
+    lng: 130.139429 
+  },
+  { 
+  id: "jr-chikuhi-JK013", 
+   name: "大入", 
+   nameEn: "Dainyu",  
+    lat: 33.508855, 
+    lng: 130.106038 
+  },
+  { 
+    id: "jr-chikuhi-JK014", 
+   name: "福吉", 
+    nameEn: "Fukuyoshi",  
+    lat: 33.501554, 
+    lng: 130.079523 
+  },
+  { 
+    id: "jr-chikuhi-JK015", 
+    name: "鹿家", 
+    nameEn: "Shikaka",  
+    lat: 33.484259, 
+    lng: 130.047476 
+  },
+  { 
+    id: "jr-chikuhi-JK016", 
+    name: "浜崎", 
+    nameEn: "Hamasaki",  
+    lat: 33.446720, 
+    lng: 130.036798 
+  },
+  { 
+    id: "jr-chikuhi-JK017", 
+    name: "虹の松原", 
+    nameEn: "Nijinomatsubara",  
+    lat: 33.441033, 
+    lng: 130.016195 
+  },
+  { 
+    id: "jr-chikuhi-JK018", 
+    name: "東唐津", 
+    nameEn: "Higashi Karatsu",  
+    lat: 33.439393, 
+    lng: 129.996837 
+  },
+  { 
+    id: "jr-chikuhi-JK019", 
+    name: "和多田", 
+    nameEn: "Watada",  
+    lat: 33.436933, 
+    lng: 129.980768 
+  },
+  { 
+    id: "jr-chikuhi-JK20", 
+    name: "唐津", 
+    nameEn: "Karatsu",  
+    lat: 33.446223, 
+    lng: 129.967587 },
+  { 
+    id: "jr-chikuhi-JK21", 
+    name: "西唐津", 
+    nameEn: "Nishi Karatsu",  
+    lat: 33.461434, 
+    lng: 129.957480 
+ }
+],
+temples = [
+  { name: "雷山千如寺 大悲王院", lat: 33.49445385812953,  lng: 130.22869414812786 },
+  { name: "妙休寺", lat: 33.5209311307555,  lng: 130.1898939676296 },
+  { name: "専念寺", lat: 33.57650344350396,  lng: 130.14364458366722 },
+  { name: "光円寺", lat: 33.5278804967468,  lng: 130.24243666267267 },
+  { name: "西光寺", lat: 33.53365519639846,  lng: 130.26194204590544 },
+  { name: "法正寺", lat: 33.55732121011789,  lng: 130.12926826562833 },
+  { name: "安楽寺", lat: 33.586188427432035,  lng: 130.16151165164985 },
+  { name: "龍国寺", lat: 33.50827262081622,  lng: 130.17230018275862 
+   }
+  ]
+  }
+};
+
+
