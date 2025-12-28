6944f2c86eefcd8de392f05a62f73fd7294c46ed
sansymeon <stephenjnelson@gmail.com>
2025-12-28 17:04:36 +0900

    Update map-data.js

--- jds/fukuoka-chuo/map-data.js
+++ jds/fukuoka-chuo/map-data.js
@@ -1,33 +1,67 @@
-.MAP_CONFIG = {
-  fukuoka-chuo: {
+window.MAP_CONFIG = {
+  fukuoka_chuo: {
     center: [33.58602, 130.37622],
     zoom: 12,
 
-churches: [
-  { name: "Daimyo Catholic Church", lat: 33.59028689597394, lng: 130.39490595063134 },
-  { name: "Fukuoka Orthodox Church 福岡ハリストス正教会", lat: 33.59896586241692,  lng: 130.4576968779313 }
-];
+    stations: [
+      {
+        id: 'fukuoka-tenjin',
+        name: '天神',
+        nameEn: 'Tenjin',
+        lat: 33.5913469503287,
+        lng: 130.39891098113026
+      },
+      {
+        id: 'fukuoka-tenjin-minami',
+        name: '天神南',
+        nameEn: 'Tenjin Minami',
+        lat: 33.588670997721565,
+        lng: 130.40265554434566
+      },
+      {
+        id: 'fukuoka-yakuin',
+        name: '薬院',
+        nameEn: 'Yakuin',
+        lat: 33.5817638907711,
+        lng: 130.40165000141508
+      }
+    ],
 
-museums: [
-  { name: "Fukuoka Art Museum", lat: 33.58411652460625, lng: 130.37958344698214 }
-];
+    churches: [
+      {
+        name: 'Daimyo Catholic Church',
+        lat: 33.59028689597394,
+        lng: 130.39490595063134
+      },
+      {
+        name: 'Fukuoka Orthodox Church 福岡ハリストス正教会',
+        lat: 33.59896586241692,
+        lng: 130.4576968779313
+      }
+    ],
 
-shrines: [
-  { name: "Terumo Shrine 光雲神社", lat: 33.597145537533365, lng: 130.37643231206792 },
-  { name: "Fukuoka Prefecture Gokoku Shrine 福岡縣護國神社", lat: 33.58171522775, lng: 130.38203472165 }
-];
+    museums: [
+      {
+        name: 'Fukuoka Art Museum',
+        lat: 33.58411652460625,
+        lng: 130.37958344698214
+      }
+    ],
 
-stations: [
-  { name: "Airpot 福岡空港駅", lat: 33.59719916364836,  lng: 130.44822439462428  },
-  { name: "Higashi-Hie 東比恵駅 Station", lat: 33.589754713543435,   lng: 130.43123418620593 },
-  { name: "Hakata 博多駅", lat: 33.589580007114,  lng: 130.42049136024366 },
-  { name: "Nakasukawabata Station", lat: 33.594395499429346,   lng: 130.4060595133835 },
-  { name: "Tenjin 天神駅", lat: 33.5913469503287,   lng: 130.39891098113026 },
-  { name: "Akasaka 赤坂駅",  lat: 33.58906127661953,   lng: 130.3906207746017 },
-  { name: "Ohori Park 大濠公園駅", lat: 33.59012812434707,  lng: 130.3788320713684 },
-  { name: "Tohjinmachi 唐人町駅", lat: 33.590145613562676,  lng: 130.36996167573926 },
-  { name: "Nishijin 西新駅", lat: 33.583574101225004, lng: 130.3592916253071  },
-  { name: "Fujisaki 藤崎駅", lat: 33.581378487964386,   lng: 130.34798479092692 },
-  { name: "Muromi 室見駅", lat: 33.58091545591748,   lng: 130.33992923325593 },
-  { name: "Meinohama 姪浜", lat: 33.583729590361415,   lng: 130.3249049548341  }
-];
+    shrines: [
+      {
+        name: 'Terumo Shrine 光雲神社',
+        lat: 33.597145537533365,
+        lng: 130.37643231206792
+      },
+      {
+        name: 'Fukuoka Prefecture Gokoku Shrine 福岡縣護國神社',
+        lat: 33.58171522775,
+        lng: 130.38203472165
+      }
+    ],
+
+    temples: [],
+    parks: []
+  }
+};
