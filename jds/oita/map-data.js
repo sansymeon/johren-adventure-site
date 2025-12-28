75c76bf2ea8db3e3f3841ab77b3dde923742334e
sansymeon <stephenjnelson@gmail.com>
2025-12-28 17:49:05 +0900

    Update map-data.js

--- jds/oita/map-data.js
+++ jds/oita/map-data.js
@@ -4,31 +4,33 @@ window.MAP_CONFIG = {
     zoom: 12,
 
     stations: [
-  { 
-   id: 'nippo-oita',
+      {
+        id: 'oita',
         name: '大分',
-        nameEn: 'Oita',      
-        lat: 33.23232801877058, 
-        lng: 131.6057111319828 
-  },
-  { 
-    id: 'nippo-beppu',
-    name: '別府',
-    nameEn: 'Beppu',       
-    lat: 33.27962926018525, 
-    lng: 131.50055717746778 
-  },
+        nameEn: 'Oita',
+        lat: 33.23232801877058,
+        lng: 131.6057111319828
+      },
+      {
+        id: 'minami-oita',
+        name: '南大分',
+        nameEn: 'Minami Oita',
+        lat: 33.21406265499788,
+        lng: 131.58629379060434
+      }
+    ],
 
-  // KYUDAI MAIN LINE (Ōita → Yufuin)
-   { 
-    id: 'kyudai-minami-oita',
-    name: '南大分',
-    nameEn: 'Minami Oita',
-    lat: 33.21406265499788, 
-    lng: 131.58629379060434 
-   }
-];
+    churches: [
+      {
+        name: 'St. Francis Xavier Cathedral',
+        lat: 33.238665,
+        lng: 131.604509
+      }
+    ],
 
-   churches: [
-  {  name: "St. Francis Xavier Cathedral", lat: 33.238665, lng: 131.604509 }
-];
+    museums: [],
+    shrines: [],
+    temples: [],
+    parks: []
+  }
+};
