9ef0b92e9e9a993bed2dae7d9d9c26b58dc1f02e
sansymeon <stephenjnelson@gmail.com>
2026-01-04 09:07:05 +0900

    Update map-data.js

--- jds/fukuoka-west/map-data.js
+++ jds/fukuoka-west/map-data.js
@@ -1,112 +1,45 @@
 window.MAP_CONFIG = {
-  itoshima: {
-    center: [33.557, 130.199],
+  fukuoka-west: {
+    center: [33.58355982784518, 130.32484060626882],
     zoom: 12,
 
-  museums: [
-  { 
-    name: "Ito Art Museum", 
-   lat: 33.56063278048929,   
-   lng: 130.20447283810256 
-  },
-  { 
-    name: "Ito-koku History Museum", 
-   lat: 33.53641274192818,   
-   lng: 130.25185578816215 
-  }
-],
+  museums: [],
   
   stations: [
-  { 
-    id: "jr-chikuhi-jk01", 
-    name: "姪浜", 
-    nameEn: "Meinohama",  
-    lat: 33.579257, 
-    lng: 130.273519 
-  },
-  { 
+   { 
     id: "jr-chikuhi-jk02", 
    name: "下山門", 
     nameEn: "Shimoyamato",  
-    lat: 33.578150, 
-    lng: 130.259861 
+    lat: 33.581928964095525,
+    lng: 130.30821717928148 
+  },
+    { 
+    id: "jr-chikuhi-jk01", 
+    name: "姪浜", 
+    nameEn: "Meinohama",  
+    lat: 33.58355982784518,  
+    lng: 130.32484060626882 
   },
   { 
-    id: "-kuko-jk05", 
-   name: "周船寺", 
-   nameEn: "Susenji",  
-   lat: 33.570792, 
-   lng: 130.246136 
+    id: "k02-muromi", 
+   name: "室見", 
+   nameEn: "Muromi",  
+   lat: 33.58093333242043,  
+   lng: 130.34004725044602 
   },
   { 
-    id: "jr-chikuhi-jk06", 
-    name: "波多江", 
-    nameEn: "Hatae",  
-    lat: 33.563944, 
-    lng: 130.226822 
+    id: "k02-fujisaki", 
+    name: "藤崎", 
+    nameEn: "Fujisaki",  
+    lat: 33.58128480587064,  
+    lng: 130.34811194647156 
   },
   { 
-    id: "jr-chikuhi-jk07", 
-   name: "糸島高校前", 
-   nameEn: "Itoshimakoko-mae",  
-   lat: 33.560660, 
-   lng: 130.213243 
-    },
-  {
-      id: "jr-chikuhi-jk08", 
-      name: "筑前前原", 
-      nameEn: "Maebaru", 
-      lat: 33.557082, 
-      lng: 130.199305 
-      },
-    { 
-      id: "jr-chikuhi-jk09", 
-      name: "美咲が丘", 
-      nameEn: "Misakigaoka", 
-      lat: 33.549959, 
-      lng: 130.185515 
-    },
-    { 
-      id: "jr-chikuhi-jk10", 
-      name: "加布里", 
-      nameEn: "Kafuri", 
-      lat: 33.543150, 
-      lng: 130.176150 
-    },
-    { 
-      id: "jr-chikuhi-jk11", 
-      name: "一貴山", 
-      nameEn: "Ikisan", 
-      lat: 33.534295, 
-      lng: 130.168242 
-    },
-    { 
-      id: "jr-chikuhi-jk12", 
-      name: "筑前深江", 
-      nameEn: "Fukae", 
-      lat: 33.514949, 
-      lng: 130.139429 
-    },
-    { 
-      id: "jr-chikuhi-jk13", 
-      name: "大入", 
-      nameEn: "Dainyu", 
-      lat: 33.508855, 
-      lng: 130.106038 
-    },
-    { 
-      id: "jr-chikuhi-jk14", 
-     name: "福吉", 
-     nameEn: "Fukuyoshi", 
-      lat: 33.501554, 
-      lng: 130.079523 
-    },
-    { 
-      id: "jr-chikuhi-jk15", 
-     name: "鹿家", 
-     nameEn: "Shikaka", 
-      lat: 33.484259, 
-      lng: 130.047476
+    id: "k02-nishijin", 
+   name: "西新", 
+   nameEn: "Nishijin",  
+   lat: 33.58362772907049,  
+   lng: 130.35954911735814 
     }
   ],
 
@@ -124,30 +57,18 @@ window.MAP_CONFIG = {
  ],
     
   churches: [
-      { name: "Itoshima Catholic Church", lat: 33.584095, lng: 130.189016 },
-      { name: "Baptist Church 福岡西部バプテスト教会", lat: 33.56349797391542, lng: 130.20519427279035 },
-      { name: "West Fukuoka Hope Christian Church 西福岡のぞみキリスト教会", lat: 33.548273234052175, lng: 130.2120380977682 },
-      { name: "Maebaru Christian Church 日本基督教団 前原教会", lat: 33.55976434044294, lng: 130.19648464789108 },
-      { name: "LDS Church 末日聖徒イエス・キリスト教会 糸島ワード", lat: 33.564099560006774, lng: 130.20782879949556 
+      { name: "Baptist Church 姪浜バプテスト教会", lat: 33.584414790218574,  lng: 130.32510369901166
       }
     ],
   
 
   shrines:  [
-  { name: "Futamigaura 二見ヶ浦 海中大鳥居", lat: 33.6393416472,  lng: 130.1971275794},
-  { name: "Sakurai Shrine　櫻井神社", lat: 33.6281710061135, lng: 130.191945877441 
+  { name: "Atago 鷲尾愛宕神社", lat: 33.586443725202805,  lng: 130.33472168452056
   }
 ],
 
   temples:  [
-  { name: "雷山千如寺 大悲王院", lat: 33.49445385812953,  lng: 130.22869414812786 },
-  { name: "妙休寺", lat: 33.5209311307555,  lng: 130.1898939676296 },
-  { name: "専念寺", lat: 33.57650344350396,  lng: 130.14364458366722 },
-  { name: "光円寺", lat: 33.5278804967468,  lng: 130.24243666267267 },
-  { name: "西光寺", lat: 33.53365519639846,  lng: 130.26194204590544 },
-  { name: "法正寺", lat: 33.55732121011789,  lng: 130.12926826562833 },
-  { name: "安楽寺", lat: 33.586188427432035,  lng: 130.16151165164985 },
-  { name: "龍国寺", lat: 33.50827262081622,  lng: 130.17230018275862 
+  { name: "Junkoji　順光寺", lat: 33.59031331862292,   lng: 130.32182965079352
    }
   ] 
   } 
