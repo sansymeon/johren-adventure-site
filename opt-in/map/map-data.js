a34bdb93550a90645f6727071989cc4d545fe41c
sansymeon <stephenjnelson@gmail.com>
2026-01-06 15:38:09 +0900

    Create map-data.js

---
+++ opt-in/map/map-data.js
@@ -0,0 +1,80 @@
+// JBS policy:
+// Only listings with `visible: true` are shown.
+// Others are pre-registered but unpublished.
+
+window.samples = [
+  { name: "Johren Itoshima サンプル・スポット", lat: 33.55857973584153, lng: 130.21385183358143, url: "/merchant/sample/index.html" },
+  { name: "Johren Karatsu サンプル・スポット", lat: 33.44973729276236,  lng: 129.96801192244578, url: "/merchant/sample/index.html" },
+  { name: "Johren Fukuoka-Chuo サンプル・スポット", lat: 33.58620293814177, lng: 130.3764331173192, url: "/merchant/sample/index.html" },
+  { name: "Johren Oita サンプル・スポット", lat: 33.23137680796785,  lng: 131.60516398591903, url: "/merchant/sample/index.html" },
+];
+
+window.artisans = [
+  { name: "ろうそく工房クレアーレ Candle Artisan", lat: 33.583231,   lng: 130.195634, visible: false
+ },
+  { name: "ガラス工房DAICHI", lat: 33.546517126200385, lng: 130.1660542259566, visible: false },
+  { name: "Antique House Perla", lat: 33.548387106215266, lng: 130.16542390442925, visible: false },
+  { name: "アトリエ·エトルリア Stained Glass", lat: 33.619045,  lng: 130.1937628211, visible: false },
+  { name: "Swing Art グラス", lat: 33.56212691996682,  lng: 130.21855634038954, visible: false }
+];
+window.baths = [
+  { name: "伊都の湯どころ", lat: 33.585778162100404, lng: 130.21362045229455, visible: false },
+  { name: "きららの湯", lat: 33.52004990493861,  lng: 130.13966841526832, visible: false },
+  { name: "まむし温泉", lat: 33.489391789689044,  lng: 130.07483899898003, visible: false },
+  { name: "喜八荘", lat: 33.50242258141895,  lng: 130.07111490263992, visible: false },
+  { name: "ななのゆ（七山温泉）", lat: 33.442961078432944,  lng: 130.12060902345107, visible: false },
+  { name: "おんくり", lat: 33.36868549291855,  lng: 130.20393675228237, visible: false }
+];
+window.bookstores = [
+  { name: "TSUTAYA 九大伊都ラボ",  lat: 33.594159,   lng: 130.229052, visible: false }
+];
+window.coffeeshops = [
+  { name: "Starbucks 九大伊都ラボ", lat: 33.593384, lng: 130.231027, visible: false  },
+   { name: "Starbucks 周船寺", lat: 33.5789063067578, lng: 130.25371607017433, visible: false  },
+  { name: "カフェ・ショパン Cafe Chopin", lat: 33.559759752030715, lng: 130.204327352293, visible: false  },
+  { name: "Hoshino Coffee", lat: 33.563034, lng: 130.215636, visible: false  }
+];
+window.combini = [
+  { name: "7-11 Itonomori", lat: 33.55982085693245,  lng: 130.21178627322553, visible: false  },
+  { name: "7-11 Arita", lat: 33.54841242658414, lng: 130.2124361059176, visible: false  },
+  { name: "7-11 元浜", lat: 33.5909784895695, lng: 130.22379108651617, visible: false  },
+  { name: "7-11 泊", lat: 33.584500539943, lng: 130.2135658717259, visible: false  },
+  { name: "Lawson Arita", lat: 33.54762202143682, lng: 130.21241581513144, visible: false  },
+  { name: "Lawson 糸島農協前共同出張所", lat: 33.561315555916,  lng: 130.2085728982, visible: false  },
+  { name: "Family Mart 波多江駅北", lat: 33.56418086878, lng: 130.22258475806444, visible: false  },
+  { name: "Family Mart 元岡", lat: 33.593901561573446, lng: 130.22838073741727, visible: false  },
+  { name: "Lawson 元岡", lat: 33.59158084138807,  lng: 130.23186347268907, visible: false  }
+];
+window.drugstores = [
+  { name: "Drug Store Mori Urashi", lat: 33.55959824540431, lng: 130.21227299570643, visible: false },
+  { name: "Cosmos Urashi", lat: 33.56266588620052,  lng: 130.21097361361203, visible: false  }
+];
+window.hotels = [
+  { name: "Glocal Hotel Itoshima", lat: 33.58398734228711,  lng: 130.21374198567526, visible: false  },
+  { name: "Karatsu Seaside Hotel", lat: 33.448514368504696,   lng: 129.99012564665264, visible: false  },
+  { name: "Karatsu Daiichi Hotel Riviere", lat: 33.44339040982554,   lng: 129.98427119640402, visible: false  },
+  { name: "Mercure Saga Karatsu Resort", lat: 33.447834269377836,    lng: 129.98748956578044, visible: false  },
+  { name: "AZ Hotel 福岡糸島", lat: 33.561399656066335,  lng: 130.2142046816784, visible: false  }
+];
+
+window.noodles = [
+  { name: "Nagahama Ramen Riki 力", lat: 33.563060, lng: 130.217424, visible: false  }
+];
+window.restaurants = [
+  { name: "カレント Current Restaurant", lat: 33.608696, lng: 130.161656, visible: false  },
+  { name: "清香園 Korean Restaurant", lat: 33.563883, lng: 130.217202, visible: false  },
+  { name: "風の丘 Yakiniku Restaurant", lat: 33.552282, lng: 130.192580, visible: false  },
+  { name: "マックドナルド MacDonalds Maebaru", lat: 33.561406, lng: 130.210648, visible: false  },
+  { name: "マックドナルド 九大学研都市店", lat: 33.578508599783994, lng: 130.25301490872266, visible: false  },
+  { name: "Bikkuri Donkey 九大学研都市店", lat: 33.57863261959046, lng: 130.25335738941635, visible: false  },
+  { name: "鉢 Hachi Steak House", lat: 33.448528, lng: 130.045132, visible: false  },
+  { name: "喜八荘", lat: 33.502444517182056,  lng: 130.0713461092523, visible: false  },
+  { name: "サンセットビーチ Beach Cafe Sunset", lat: 33.641949, lng: 130.201588, visible: false  }
+];
+
+window.supermarkets = [
+  { name: "Sunny Supermaket", lat: 33.561620, lng: 130.212912, visible: false  },
+  { name: "ITOSAISAI Farmer's Market", lat: 33.555881770215,  lng: 130.22828993782352, visible: false  }
+];  
+
+ 
