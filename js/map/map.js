d0ecde35001fc89cd278ecae4bd27415cc17adbb
sansymeon <stephenjnelson@gmail.com>
2026-01-12 07:40:47 +0900

    Update map.js

--- js/map/map.js
+++ js/map/map.js
@@ -39,8 +39,11 @@
   // -------------------------------
   // MAP INITIALIZATION
   // -------------------------------
-  const map = L.map('map', { zoomControl: false })
-    .setView(center, zoom);
+  const map = L.map('map', {
+  zoomControl: false,
+  attributionControl: false
+}).setView(center, zoom);
+;
 
   L.control.zoom({ position: 'topright' }).addTo(map);
 
