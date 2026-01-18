aa77c29a8e12bd67992018bb86c3a7591abdb7fd
sansymeon <stephenjnelson@gmail.com>
2026-01-18 14:26:23 +0900

    Update map-data.js

--- jbs/map-data.js
+++ jbs/map-data.js
@@ -19,24 +19,25 @@ window.PLACE_MAP = {
 window.samples = [
   {
     id: "sample_01",
-    name: "Johren Itoshima サンプル・スポット",
-    level: 3,
-    lat: 33.55857973584153,
-    lng: 130.21385183358143,
+    name: "Johren Karatsu サンプル・スポット",
+    level: 2,
+    lat: 33.44973729276236,
+    lng: 129.96801192244578,
     visible: true,
     pin_url: "/jbs/pin/level_01/?id=sample_01",
     qr_url:  "/jbs/qr/level_01/?id=sample_01"
   },
   {
     id: "sample_02",
-    name: "Johren Karatsu サンプル・スポット",
-    level: 2,
-    lat: 33.44973729276236,
-    lng: 129.96801192244578,
+    name: "Johren Itoshima サンプル・スポット",
+    level: 3,
+    lat: 33.55857973584153,
+    lng: 130.21385183358143,
     visible: true,
     pin_url: "/jbs/pin/level_02/?id=sample_02",
     qr_url:  "/jbs/qr/level_02/?id=sample_02"
   },
+  
   {
     id: "sample_03",
     name: "Johren Fukuoka-Chuo サンプル・スポット",
