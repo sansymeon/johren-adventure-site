53081866f62afba395eede266667646731cacd62
sansymeon <stephenjnelson@gmail.com>
2026-01-18 14:58:41 +0900

    Update map-data.js

--- jbs/map-data.js
+++ jbs/map-data.js
@@ -16,36 +16,3 @@ window.PLACE_MAP = {
   ]
 };
 
-window.samples = [
-  {
-    id: "sample_01",
-    name: "Johren Karatsu サンプル・スポット",
-    level: 1, 
-    lat: 33.453496,
-    lng: 129.978192,
-    visible: true,
-    pin_url: "/jbs/pin/level_01/?id=sample_01",
-    qr_url:  "/jbs/qr/level_01/?id=sample_01"
-  },
-  {
-    id: "sample_02",
-    name: "Johren Itoshima サンプル・スポット",
-    level: 3,
-    lat: 33.55857973584153,
-    lng: 130.21385183358143,
-    visible: true,
-    pin_url: "/jbs/pin/level_02/?id=sample_02",
-    qr_url:  "/jbs/qr/level_02/?id=sample_02"
-  },
-  
-  {
-    id: "sample_03",
-    name: "Johren Fukuoka-Chuo サンプル・スポット",
-    level: 1,
-    lat: 33.58620293814177,
-    lng: 130.3764331173192,
-    visible: true,
-    pin_url: "/jbs/pin/level_03/?id=sample_03",
-    qr_url:  "/jbs/qr/level_03/?id=sample_03"
-  }
-];
