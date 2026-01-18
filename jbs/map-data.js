8124f0efbde6db9ca7c965ae7855dbff5e7fadea
sansymeon <stephenjnelson@gmail.com>
2026-01-18 14:22:40 +0900

    Update map-data.js

--- jbs/map-data.js
+++ jbs/map-data.js
@@ -24,8 +24,8 @@ window.samples = [
     lat: 33.55857973584153,
     lng: 130.21385183358143,
     visible: true,
-    pin_url: "/jbs/pin/level_03/?id=sample_01",
-    qr_url:  "/jbs/qr/level_03/?id=sample_01"
+    pin_url: "/jbs/pin/level_01/?id=sample_01",
+    qr_url:  "/jbs/qr/level_01/?id=sample_01"
   },
   {
     id: "sample_02",
@@ -44,7 +44,7 @@ window.samples = [
     lat: 33.58620293814177,
     lng: 130.3764331173192,
     visible: true,
-    pin_url: "/jbs/pin/level_01/?id=sample_03",
-    qr_url:  "/jbs/qr/level_01/?id=sample_03"
+    pin_url: "/jbs/pin/level_03/?id=sample_03",
+    qr_url:  "/jbs/qr/level_03/?id=sample_03"
   }
 ];
