5eef982e908822a073f220d8fb62c14dd1c12147
sansymeon <stephenjnelson@gmail.com>
2026-01-22 20:53:48 +0900

    Update map-data.js

--- jds/kyushu/map-data.js
+++ jds/kyushu/map-data.js
@@ -2,6 +2,218 @@ window.MAP_CONFIG = {
   kyushu: {
     center: [33.316994,130.938711],
     zoom: 8,
+
+    churches: [
+     {
+        id: "otaru_orthodox",
+        name: "小樽ハリストス正教会・主の復活聖堂",
+        nameEn: "Otaru Orthodox Church",
+        lat: 43.19058532675394,                     
+        lng: 140.98899218173514
+      },
+      {
+        id: "sapporo_orthodox",
+        name: "札幌ハリストス正教会・主の顕栄聖堂",
+        nameEn: "Sapporo Orthodox Church",
+        lat: 43.01865151311119,                      
+        lng: 141.40312645103944
+      },
+      {
+        id: "otaru_orthodox",
+        name: "小樽ハリストス正教会・主の復活聖堂",
+        nameEn: "Otaru Orthodox Church",
+        lat: 43.19058532675394,                     
+        lng: 140.98899218173514
+      },
+      {
+        id: "tomakomai_orthodox",
+        name: "苫小牧ハリストス正教会・主の降誕聖堂",
+        nameEn: "Tomakomai Orthodox Church",
+        lat: 42.642654755289584,                    
+        lng: 141.5805366375196
+      }, 
+      {
+        id: "kushiro_orthodox",
+        name: "釧路ハリストス正教会・聖神゜降臨聖堂",
+        nameEn: "Kushiro Orthodox Church",
+        lat: 42.97550972265846,                    
+        lng: 144.38810002404912
+      }, 
+      {
+        id: "kamimusa_orthodox",
+        name: "上武佐ハリストス正教会・生神女就寝聖堂",
+        nameEn: "Kamimusa Orthodox Church",
+        lat: 43.61850720398041,                   
+        lng: 144.99488426642338
+      }, 
+      {
+        id: "shari_orthodox",
+        name: "斜里ハリストス正教会・生神女福音聖堂",
+        nameEn: "Shari Orthodox Church",
+        lat: 43.899288709566385,                  
+        lng: 144.62383363760782
+      }, 
+      {
+        id: "morioka_orthodox",
+        name: "盛岡ハリストス正教会・聖十字架挙栄聖堂",
+        nameEn: "Morioka Orthodox Church",
+        lat: 39.718873261561406,                 
+        lng: 141.14334543732312
+      },
+      {
+        id: "hokuroku_orthodox",
+        name: "北鹿ハリストス正教会・生神女福音会堂",
+        nameEn: "Hokuroku Orthodox Church",
+        lat: 40.21460725011423,                
+        lng: 140.62425246619102
+      },
+      {
+        id: "iwayado_orthodox",
+        name: "岩谷堂ハリストス正教会・主の降誕聖堂",
+        nameEn: "Iwayado Orthodox Church",
+        lat: 39.19564918643734,               
+        lng: 141.1774628084538
+      },
+      {
+        id: "yamada_orthodox",
+        name: "山田ハリストス正教会・主の復活会堂",
+        nameEn: "Yamada Orthodox Church",
+        lat: 39.46703315417506,              
+        lng: 141.9510394814841
+      },
+      {
+        id: "tonosei_orthodox",
+        name: "遠野正教会・聖太祖アウラアム・サッラ会堂",
+        nameEn: "Tonosei Orthodox Church",
+        lat: 39.34801785518456,              
+        lng: 141.56757262380552
+      },{
+        id: "ichinoseki_orthodox",
+        name: "一関ハリストス正教会・昇天聖堂",
+        nameEn: "Ichinoseki Orthodox Church",
+        lat: 38.92259105365958,             
+        lng: 141.1217309986392
+      },
+      {
+        id: "kesennuma_orthodox",
+        name: "気仙沼ハリストス正教会・復活会堂",
+        nameEn: "Kesennuma Orthodox Church",
+        lat: 38.906254620638066,             
+        lng: 141.56940059494167
+      },
+      {
+        id: "kannari_orthodox",
+        name: "金成ハリストス正教会・聖使徒イオアン聖堂",
+        nameEn: "Kannari Orthodox Church",
+        lat: 38.810866974763925,            
+        lng: 141.07472281853876
+      },
+      {
+        id: "jumonji_orthodox",
+        name: "十文字ハリストス正教会",
+        nameEn: "Jumonji Orthodox Church",
+        lat: 38.767362271760085, 141.14312620047744           
+        lng: 141.14312620047744
+      },
+      {
+        id: "kyoto_orthodox",
+        name: "京都ハリストス正教会",
+        nameEn: "Kyoto Orthodox Church",
+        lat: 35.01446594680803,           
+        lng: 135.76419887751453
+      },
+      {
+        id: "iwate_orthodox",
+        name: "盛ハリストス正教会・昇天聖堂",
+        nameEn: "Iwate Orthodox Church",
+        lat: 39.08736041329395,            
+        lng: 141.70685229327933
+      },
+      {
+        id: "nagoya_orthodox",
+        name: "名古屋ハリストス正教会・神現聖堂",
+        nameEn: "Holy Theophany Orthodox Church",
+        lat: 34.76802881935057,          
+        lng: 137.39393794866496
+      },
+      {
+        id: "toyohashi_orthodox",
+        name: "豊橋ハリストス正教会・聖使徒福音者マトフェイ聖堂",
+        nameEn: "Toyohashi Orthodox Church",
+        lat: 34.76802881935057,          
+        lng: 137.39393794866496
+      },
+      {
+        id: "handa_orthodox",
+        name: "半田ハリストス正教会・聖イオアン・ダマスキン聖堂",
+        nameEn: "Saint John of Damascus Orthodox Church",
+        lat: 34.91164457680192,         
+        lng: 136.9408163370278
+      },
+      {
+        id: "osaka_orthodox",
+        name: "大阪ハリストス正教会・生神女庇護聖堂",
+        nameEn: "Osaka Orthodox Church",
+        lat: 34.76633139508822,        
+        lng: 135.511802550513
+      },
+      {
+        id: "kobe_orthodox",
+        name: "神戸ハリストス正教会",
+        nameEn: "Dormition Orthodox Church",
+        lat: 34.69910459077482,       
+        lng: 135.1920814648509
+      },
+      {
+        id: "tokushima_orthodox",
+        name: "徳島ハリストス正教会･聖神降臨聖堂",
+        nameEn: "Tokushima Orthodox Church",
+        lat: 34.043576063212164,      
+        lng: 134.56363589464922
+      },
+      {
+        id: "yanaihara_orthodox",
+        name: "柳井原ハリストス正教会",
+        nameEn: "Yanaihara Orthodox Church",
+        lat: 34.60898209432798,     
+        lng: 133.72219075235222
+      },
+      {
+        id: "tokushima_orthodox",
+        name: "徳島ハリストス正教会･聖神降臨聖堂",
+        nameEn: "Tokushima Orthodox Church",
+        lat: 34.043597041074776,    
+        lng: 134.56363952705485
+      },
+      {
+        id: "fukuoka_orthodox",
+        name: "九州北ハリストス正教会",
+        nameEn: "Fukuoka Orthodox Church",
+        lat: 33.59902883402787, 
+        lng: 130.4577854597739
+      },
+      {
+        id: "hitoyoshi_orthodox",
+        name: "人吉ハリストス正教会・生神女庇護聖堂",
+        nameEn: "Hitoyoshi Orthodox Church",
+        lat: 32.21672654292169, 
+        lng: 130.76909883687796
+      },
+      {
+        id: "kumamoto_orthodox",
+        name: "熊本ハリストス正教会",
+        nameEn: "Kumamoto Orthodox Church",
+        lat: 32.80435218639175,   
+        lng: 130.71799555031276
+      },
+    {
+        id: "kagoshima_orthodox",
+        name: "鹿児島ハリストス正教会・聖使徒イアコフ聖堂",
+        nameEn: "Kagoshima Orthodox Church",
+        lat: 31.591379029899404,  
+        lng: 130.54791936752866
+      },   
+    ],
    
   stations: [
 // CHIKUHI LINE
