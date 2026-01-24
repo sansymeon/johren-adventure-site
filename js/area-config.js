6d6d7bbda0f866f18f9ee42986c0926623fcf594
sansymeon <stephenjnelson@gmail.com>
2026-01-24 13:27:24 +0900

    Create area-config.js

---
+++ js/area-config.js
@@ -0,0 +1,14 @@
+window.MAP_CONFIG = window.MAP_CONFIG || {};
+
+window.MAP_CONFIG.KK_GAYA = {
+  center: [5.9826, 116.0737],
+  zoom: 16,
+  stations: [],
+
+  // put your places here for now:
+  parks: [
+    { id:"kk_gaya_daily_coffee", name:"Daily Coffee", nameEn:"Daily Coffee", lat: 5.9829, lng: 116.0732 },
+    { id:"kk_gaya_5star_chicken", name:"5 Star Chicken", nameEn:"5 Star Chicken", lat: 5.9824, lng: 116.0739 },
+    // ...
+  ]
+};
