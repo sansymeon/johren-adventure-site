5a62c3364c93334ba1600db57c9e38a9ca3ab2cb
sansymeon <stephenjnelson@gmail.com>
2026-01-24 17:03:07 +0900

    Update map.js

--- js/map/map.js
+++ js/map/map.js
@@ -174,7 +174,7 @@
     const el = document.getElementById('pinFilters');
     if (!el) return;
 
-    const labels = { coffee:"Coffee", restaurant:"Food", supermarket:"Shop", church:"Church", mosque:"Mosque"};
+    const labels = { coffee:"Coffee", restaurant:"Food", supermarket:"Shop", church:"Church", museum: "Museum", mosque:"Mosque"};
 
     el.innerHTML = types.map(t => `
       <label>
