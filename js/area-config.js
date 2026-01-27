618d98dc39e8c34c5511f0a3b121cd27f96e473d
sansymeon <stephenjnelson@gmail.com>
2026-01-27 18:53:34 +0900

    Update area-config.js

--- js/area-config.js
+++ js/area-config.js
@@ -268,6 +268,7 @@ window.MAP_CONFIG.ITOSHIMA = {
       id: "jr-chikuhi-jk16", 
       name: "浜崎", 
       nameEn: "Hamasaki", 
+      type: "station",
       lat: 33.44672, 
       lng: 130.036798 
       },
@@ -275,6 +276,7 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "jr-chikuhi-jk17", 
        name: "虹の松原", 
        nameEn: "Nijinomatsubara", 
+        type: "station",
         lat: 33.441033, 
         lng: 130.01619 
       },
@@ -282,13 +284,17 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "jr-chikuhi-jk18", 
        name: "東唐津", 
        nameEn: "Higashi Karatsu", 
+         type: "station",
         lat: 33.439393, 
         lng: 129.996837 
+  
       },
+    
       { 
         id: "jr-chikuhi-jk19", 
        name: "和多田", 
         nameEn: "Watada", 
+         type: "station",
         lat: 33.436933, 
         lng: 129.980768 
       },
@@ -296,13 +302,15 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "jr-chikuhi-jk20", 
        name: "唐津", 
        nameEn: "Karatsu", 
-       lat: 33.446223, 
+        type: "station",
+        lat: 33.446223, 
        lng: 129.967587 
       },
       { 
         id: "jr-chikuhi-jk21", 
        name: "西唐津", 
        nameEn: "Nishi Karatsu", 
+         type: "station",
         lat: 33.461434, 
         lng: 129.95748 
       },
@@ -310,20 +318,23 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "jr-karatsu-yamamoto", 
        name: "山本", 
        nameEn: "Yamamoto", 
+        type: "station",
         lat: 33.39278208271683, 
         lng: 129.98118155167737 
       },
       { 
         id: "jr-karatsu-ouchi", 
        name: "相知", nameEn: "Ouchi", 
-       lat: 33.34699039849446, 
+        type: "station",
+        lat: 33.34699039849446, 
         lng: 130.01952634118035 
       },
       { 
         id: "jr-karatsu-iwaya", 
        name: "岩屋", 
         nameEn: "Iwaya", 
-       lat: 33.33076505677805, 
+        type: "station",
+        lat: 33.33076505677805, 
         lng: 130.03827683163834 
       }
     ],
@@ -333,6 +344,7 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "karatsu_catholic",
         name: "カトリック唐津教会",      
         nameEn: "Karatsu Catholic Church",
+         type: "church",
         lat: 33.45252801155086,            
         lng: 129.9614152412968
       }
@@ -343,6 +355,7 @@ window.MAP_CONFIG.ITOSHIMA = {
          id: "hikiyama_hall"
        name: "曳山展示場", 
        nameEn: "Hikiyama Exhibition Hall", 
+        type: "museum",
        lat: 33.445946414612074, 
        lng: 129.96991124129468 
       },
@@ -350,14 +363,16 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "karatsu_castle"
         name: "唐津城", 
        nameEn: "Karatsu Castle", 
-       lat: 33.45345765716856, 
+        type: "museum",
+          lat: 33.45345765716856, 
        lng: 129.9782080418245 
       },
       { 
         id: "takatori_museum"
         name: "旧高取邸", 
        nameEn: "Former Takatori Residence", 
-       lat: 33.45437629769318, 
+        type: "museum",
+          lat: 33.45437629769318, 
        lng: 129.97209266442803 
       }
     ],
@@ -367,14 +382,16 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "karatsu_shrine"
         name: "唐津神社", 
        nameEn: "Karatsu Shrine", 
-       lat: 33.45227410257489, 
+        type: "shrine",
+      lat: 33.45227410257489, 
        lng: 129.96948404957564 
       },
       { 
         id: "myoken_shrine"
         name: "妙見神社", 
        nameEn: "Myoken Shrine", 
-       lat: 33.46854559781762, 
+         type: "shrine",
+          lat: 33.46854559781762, 
        lng: 129.95317852240166 
       }
     ],
@@ -384,7 +401,8 @@ window.MAP_CONFIG.ITOSHIMA = {
         id: "gonshoji_temple"
         name: "近松寺", 
        nameEn: "Gonshoji Temple", 
-       lat: 33.449901908390125, 
+         type: "temple",
+      lat: 33.449901908390125, 
        lng: 129.96520797775324 
       }
     ],
