9287e588bf07b93fe4d7010c06217efd3fe91f39
sansymeon <stephenjnelson@gmail.com>
2026-01-29 22:06:59 +0900

    Update map-data.js

--- locations/japan/hokkaido/map-data.js
+++ locations/japan/hokkaido/map-data.js
@@ -5,13 +5,77 @@ window.MAP_CONFIG = {
 
     pins:  [
   {
-        id: "miyazaki_catholic",
-        name: "カトリック宮崎教会",
-        nameEn: "Miyazaki Catholic Church",
+        id: "otaru_orthodox",
+        name: "小樽ハリストス正教会・主の復活聖堂",
+        nameEn: "Otaru Orthodox Church",
+         type: "church",
+        lat: 43.19058532675394,                     
+        lng: 140.98899218173514
+      },
+      {
+        id: "sapporo_orthodox",
+        name: "札幌ハリストス正教会・主の顕栄聖堂",
+        nameEn: "Sapporo Orthodox Church",
+         type: "church",
+        lat: 43.01865151311119,                      
+        lng: 141.40312645103944
+      },
+      {
+        id: "otaru_orthodox",
+        name: "小樽ハリストス正教会・主の復活聖堂",
+        nameEn: "Otaru Orthodox Church",
+         type: "church",
+        lat: 43.19058532675394,                     
+        lng: 140.98899218173514
+      },
+      {
+        id: "tomakomai_orthodox",
+        name: "苫小牧ハリストス正教会・主の降誕聖堂",
+        nameEn: "Tomakomai Orthodox Church",
+         type: "church",
+        lat: 42.642654755289584,                    
+        lng: 141.5805366375196
+      }, 
+      {
+        id: "kushiro_orthodox",
+        name: "釧路ハリストス正教会・聖神゜降臨聖堂",
+        nameEn: "Kushiro Orthodox Church",
+         type: "church",
+        lat: 42.97550972265846,                    
+        lng: 144.38810002404912
+      }, 
+      {
+        id: "kamimusa_orthodox",
+        name: "上武佐ハリストス正教会・生神女就寝聖堂",
+        nameEn: "Kamimusa Orthodox Church",
+         type: "church",
+        lat: 43.61850720398041,                   
+        lng: 144.99488426642338
+      }, 
+      {
+        id: "shari_orthodox",
+        name: "斜里ハリストス正教会・生神女福音聖堂",
+        nameEn: "Shari Orthodox Church",
+         type: "church",
+        lat: 43.899288709566385,                  
+        lng: 144.62383363760782
+      }, 
+      {
+        id: "morioka_orthodox",
+        name: "盛岡ハリストス正教会・聖十字架挙栄聖堂",
+        nameEn: "Morioka Orthodox Church",
+         type: "church",
+        lat: 39.718873261561406,                 
+        lng: 141.14334543732312
+      },
+      {
+        id: "hokuroku_orthodox",
+        name: "北鹿ハリストス正教会・生神女福音会堂",
+        nameEn: "Hokuroku Orthodox Church",
         type: "church",
-        lat: 31.918897135229283,   
-        lng: 131.44679345794682
-      },   
+        lat: 40.21460725011423,                
+        lng: 140.62425246619102
+      },
       {
   id: "unknown_sapporo",
   line: "unknown",
