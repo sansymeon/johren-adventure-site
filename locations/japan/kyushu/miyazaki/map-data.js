476048a44fe4bc1ab8890b4216c310c1ebd2e961
sansymeon <stephenjnelson@gmail.com>
2026-01-29 14:53:06 +0900

    Create map-data.js

---
+++ locations/japan/kyushu/miyazaki/map-data.js
@@ -0,0 +1,26 @@
+window.MAP_CONFIG = {
+  miyazaki: {
+    center: [31.909571456708782, 131.42017898421975],
+    zoom: 10,
+
+    pins:  [
+  {
+        id: "miyazaki_catholic",
+        name: "カトリック宮崎教会",
+        nameEn: "Miyazaki Catholic Church",
+        type: "church",
+        lat: 31.918897135229283,   
+        lng: 131.44679345794682
+      },   
+      {
+  id: "unknown_miyazaki",
+  line: "unknown",
+  name: "宮崎",
+  nameEn: "Miyazaki",
+  type: "station",
+  lat: 31.91563158122375, 
+  lng: 131.43231940518467
+ }
+  ]
+  }
+};
