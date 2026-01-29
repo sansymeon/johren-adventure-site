7558c547242b14325bff096f176d9c70fe020066
sansymeon <stephenjnelson@gmail.com>
2026-01-29 22:04:23 +0900

    Create map-data.js

---
+++ locations/japan/hokkaido/map-data.js
@@ -0,0 +1,26 @@
+window.MAP_CONFIG = {
+  sapporo: {
+    center: [43.068772070661566, 141.3509750091167],
+    zoom: 10,
+
+    pins:  [
+  {
+        id: "miyazaki_catholic",
+        name: "カトリック宮崎教会",
+        nameEn: "Miyazaki Catholic Church",
+        type: "church",
+        lat: 31.918897135229283,   
+        lng: 131.44679345794682
+      },   
+      {
+  id: "unknown_sapporo",
+  line: "unknown",
+  name: "札幌",
+  nameEn: "Sapporo",
+  type: "station",
+  lat: 43.068772070661566, , 
+  lng: 141.3509750091167
+ }
+  ]
+  }
+};
