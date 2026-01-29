0cbe72c9035bf3ff6df2fdcb05c82fad75cb74f3
sansymeon <stephenjnelson@gmail.com>
2026-01-29 09:25:57 +0900

    Update map-data.js

--- locations/japan/kyushu/fukuoka-west/map-data.js
+++ locations/japan/kyushu/fukuoka-west/map-data.js
@@ -1,74 +1,68 @@
-window.MAP_CONFIG = {
-  "fukuoka-west": {
+window.MAP_CONFIG.fukuoka_west = {
     center: [33.58355982784518, 130.32484060626882],
     zoom: 12,
 
-   stations: [
-      { 
-        id: "jr-chikuhi-jk02", 
+ pins:  [
+        {
+      id: "chikuhi-shimoyamato", 
         name: "下山門", 
         nameEn: "Shimoyamato",  
-        lat: 33.581928964095525,
+        type: "station",
+          lat: 33.581928964095525,
         lng: 130.30821717928148 
       },
       { 
-        id: "jr-chikuhi-jk01", 
+        id: "chikuhi-meinohama", 
         name: "姪浜", 
         nameEn: "Meinohama",  
-        lat: 33.58355982784518,  
+         type: "station",
+         lat: 33.58355982784518,  
         lng: 130.32484060626882 
       },
       { 
-        id: "k02-muromi", 
+        id: "airport-muromi", 
         name: "室見", 
         nameEn: "Muromi",  
-        lat: 33.58093333242043,  
+         type: "station",
+         lat: 33.58093333242043,  
         lng: 130.34004725044602 
       },
       { 
-        id: "k03-fujisaki", 
+        id: "airport-fujisaki", 
         name: "藤崎", 
         nameEn: "Fujisaki",  
-        lat: 33.58128480587064,  
+         type: "station",
+         lat: 33.58128480587064,  
         lng: 130.34811194647156 
       },
       { 
-        id: "k04-nishijin", 
+        id: "airport-nishijin", 
         name: "西新", 
         nameEn: "Nishijin",  
-        lat: 33.58362772907049,  
+         type: "station",
+         lat: 33.58362772907049,  
         lng: 130.35954911735814 
-      }
-    ],
-
-    
-
-
-    churches: [
+      },
       {
         name: "姪浜バプテスト教会",
         nameEn: "Meinohama Baptist Church",
-        lat: 33.584414790218574,
+         type: "church",
+         lat: 33.584414790218574,
         lng: 130.32510369901166
-      }
-    ],
-
-    shrines: [
-      {
+      },
+      { 
         name: "鷲尾愛宕神社",
         nameEn: "Atago Shrine", 
-        lat: 33.586443725202805,
+         type: "shrine",
+         lat: 33.586443725202805,
         lng: 130.33472168452056
-      }
-    ],
-
-    temples: [
-      {
+      },
+      { 
         name: "順光寺",
-        nameEn: "Junkoji", 
-        lat: 33.59031331862292,
+        nameEn: "Junkoji Temple", 
+         type: "temple",
+         lat: 33.59031331862292,
         lng: 130.32182965079352
-      }
-    ]
-  }
-};
+        }
+ ]
+    };
