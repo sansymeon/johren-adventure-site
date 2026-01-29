b7c888c087b6fa4e1be507929e8a600c4def16f7
sansymeon <stephenjnelson@gmail.com>
2026-01-29 15:27:18 +0900

    Create map-data.js

---
+++ locations/japan/kyushu/saga/saga-city/map-data.js
@@ -0,0 +1,26 @@
+window.MAP_CONFIG = {
+  saga: {
+    center: [33.264770100730864, 130.30114877828134],
+    zoom: 10,
+
+    pins:  [
+  {
+        id: "saga_catholic",
+        name: "カトリック佐賀教会",
+        nameEn: "Saga Catholic Church",
+        type: "church",
+        lat: 33.253299199812,   
+        lng: 130.30116824247887
+      },   
+      {
+  id: "unknown_saga",
+  line: "unknown",
+  name: "佐賀",
+  nameEn: "saga",
+  type: "station",
+  lat: 33.26416730255144, 
+  lng: 130.29694618351647
+ }
+  ]
+  }
+};
