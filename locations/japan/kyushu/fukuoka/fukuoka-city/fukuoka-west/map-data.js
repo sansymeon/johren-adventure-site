318d13999624b034f976c91e5e7f38c5d143cdc4
sansymeon <stephenjnelson@gmail.com>
2026-01-29 12:43:38 +0900

    Create map-data.js

---
+++ locations/japan/kyushu/fukuoka/fukuoka-city/fukuoka-west/map-data.js
@@ -0,0 +1,70 @@
+window.MAP_CONFIG = {
+  fukuoka_west: {
+    center: [33.58355982784518, 130.32484060626882],
+    zoom: 12,
+
+ pins:  [
+        {
+      id: "chikuhi-shimoyamato", 
+        name: "下山門", 
+        nameEn: "Shimoyamato",  
+        type: "station",
+          lat: 33.581928964095525,
+        lng: 130.30821717928148 
+      },
+      { 
+        id: "chikuhi-meinohama", 
+        name: "姪浜", 
+        nameEn: "Meinohama",  
+         type: "station",
+         lat: 33.58355982784518,  
+        lng: 130.32484060626882 
+      },
+      { 
+        id: "airport-muromi", 
+        name: "室見", 
+        nameEn: "Muromi",  
+         type: "station",
+         lat: 33.58093333242043,  
+        lng: 130.34004725044602 
+      },
+      { 
+        id: "airport-fujisaki", 
+        name: "藤崎", 
+        nameEn: "Fujisaki",  
+         type: "station",
+         lat: 33.58128480587064,  
+        lng: 130.34811194647156 
+      },
+      { 
+        id: "airport-nishijin", 
+        name: "西新", 
+        nameEn: "Nishijin",  
+         type: "station",
+         lat: 33.58362772907049,  
+        lng: 130.35954911735814 
+      },
+      {
+        name: "姪浜バプテスト教会",
+        nameEn: "Meinohama Baptist Church",
+         type: "church",
+         lat: 33.584414790218574,
+        lng: 130.32510369901166
+      },
+      { 
+        name: "鷲尾愛宕神社",
+        nameEn: "Atago Shrine", 
+         type: "shrine",
+         lat: 33.586443725202805,
+        lng: 130.33472168452056
+      },
+      { 
+        name: "順光寺",
+        nameEn: "Junkoji Temple", 
+         type: "temple",
+         lat: 33.59031331862292,
+        lng: 130.32182965079352
+           }
+  ]
+  }
+};
