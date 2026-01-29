15454ff62abe967a53e7db9f60da560e0c78add2
sansymeon <stephenjnelson@gmail.com>
2026-01-29 21:40:51 +0900

    Create map-data.js

---
+++ locations/japan/kyushu/nagasaki/map-data.js
@@ -0,0 +1,34 @@
+window.MAP_CONFIG = {
+  nagasaki: {
+    center: [32.752546298028726, 129.86865881602043],
+    zoom: 10,
+
+    pins:  [
+  {
+        id: "peace_park",
+        name: "平和公園",
+        nameEn: "Peace Park Nagasaki",
+        type: "museum",
+        lat: 32.776992537763874,    
+        lng: 129.86393524060455
+      },  
+        {
+        id: "urakami_cathedral",
+        name: "カトリック浦上教会（浦上天主堂）",
+        nameEn: "Urakami Cathedral",
+        type: "church",
+        lat: 32.77611633657394,   
+        lng: 129.86835109327734
+      }, 
+      {
+  id: "unknown_nagasaki",
+  line: "unknown",
+  name: "長崎",
+  nameEn: "Nagasaki",
+  type: "station",
+  lat: 32.752546298028726, 
+  lng: 129.86865881602043
+ }
+  ]
+  }
+};
