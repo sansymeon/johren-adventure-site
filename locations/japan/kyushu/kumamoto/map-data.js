00fb495146c02b199296d4a09f404b0870de9702
sansymeon <stephenjnelson@gmail.com>
2026-01-29 15:02:26 +0900

    Update map-data.js

--- locations/japan/kyushu/kumamoto/map-data.js
+++ locations/japan/kyushu/kumamoto/map-data.js
@@ -1,1 +1,26 @@
-.txt
+window.MAP_CONFIG = {
+  kumamoto: {
+    center: [32.80557603104838, 130.70550853156524],
+    zoom: 10,
+
+    pins:  [
+  {
+        id: "kumamoto_castle",
+        name: "熊本城",
+        nameEn: "Kumamoto Castle",
+        type: "museum",
+        lat: 32.80557603104838,    
+        lng: 130.70550853156524
+      },   
+      {
+  id: "unknown_miyazaki",
+  line: "unknown",
+  name: "宮崎",
+  nameEn: "Miyazaki",
+  type: "station",
+  lat: 31.91563158122375, 
+  lng: 131.43231940518467
+ }
+  ]
+  }
+};
