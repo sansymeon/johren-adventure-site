72dce317335022517294d37f1e90ea353a83c079
sansymeon <stephenjnelson@gmail.com>
2026-01-29 21:46:34 +0900

    Update map-data.js

--- locations/japan/kyushu/nagasaki/map-data.js
+++ locations/japan/kyushu/nagasaki/map-data.js
@@ -21,6 +21,38 @@ window.MAP_CONFIG = {
         lng: 129.86835109327734
       }, 
       {
+  id: "unknown_shin_omura",
+  line: "unknown",
+  name: "新大村",
+  nameEn: "Shin-Omura",
+  lat: 32.932626750179075,
+  lng: 129.95722857374489
+},
+{
+  id: "unknown_isahaya",
+  line: "unknown",
+  name: "諫早",
+  nameEn: "Isahaya",
+  lat: 32.85203767364156,
+  lng: 130.0412546541023
+},
+{
+  id: "unknown_aino",
+  line: "unknown",
+  name: "愛野",
+  nameEn: "Aino",
+  lat: 32.81895137075822,
+  lng: 130.15604553458053
+},
+{
+  id: "unknown_shimabara",
+  line: "unknown",
+  name: "島原",
+  nameEn: "Shimabara",
+  lat: 32.79027786498945,
+  lng: 130.370631075974
+},
+      {
   id: "unknown_nagasaki",
   line: "unknown",
   name: "長崎",
