910ea0ed3b35e5a9ff7e58fe2fb7473d17fdde40
sansymeon <stephenjnelson@gmail.com>
2026-02-01 16:07:34 +0900

    Update map-data.js

--- locations/japan/tohoku/aomori/map-data.js
+++ locations/japan/tohoku/aomori/map-data.js
@@ -1,69 +1,25 @@
 window.MAP_CONFIG = {
-  nagasaki: {
-    center: [32.752546298028726, 129.86865881602043],
+  aomori: {
+    center: [40.68810143826632, 140.81970231744864],
     zoom: 10,
 
     pins:  [
-  {
-        id: "peace_park",
-        name: "平和公園",
-        nameEn: "Peace Park Nagasaki",
-        type: "museum",
-        lat: 32.776992537763874,    
-        lng: 129.86393524060455
-      },  
         {
-        id: "urakami_cathedral",
-        name: "カトリック浦上教会（浦上天主堂）",
-        nameEn: "Urakami Cathedral",
+        id: "honcho_catholic",
+        name: "カトリック本町教会",
+        nameEn: "Honcho Catholic Church",
         type: "church",
-        lat: 32.77611633657394,   
-        lng: 129.86835109327734
+        lat: 40.8272103975467,    
+        lng: 140.75176595596236
       }, 
       {
-  id: "unknown_shin_omura",
+  id: "unknown_aomori",
   line: "unknown",
-  name: "新大村",
-  nameEn: "Shin-Omura",
+  name: "青森",
+  nameEn: "Aomori",
   type: "station",
-  lat: 32.932626750179075,
-  lng: 129.95722857374489
-},
-{
-  id: "unknown_isahaya",
-  line: "unknown",
-  name: "諫早",
-  nameEn: "Isahaya",
-  type: "station",
-  lat: 32.85203767364156,
-  lng: 130.0412546541023
-},
-{
-  id: "unknown_aino",
-  line: "unknown",
-  name: "愛野",
-  nameEn: "Aino",
-  type: "station",
-  lat: 32.81895137075822,
-  lng: 130.15604553458053
-},
-{
-  id: "unknown_shimabara",
-  line: "unknown",
-  name: "島原",
-  type: "station",
-  nameEn: "Shimabara",
-  lat: 32.79027786498945,
-  lng: 130.370631075974
-},
-      {
-  id: "unknown_nagasaki",
-  line: "unknown",
-  name: "長崎",
-  nameEn: "Nagasaki",
-  type: "station",
-  lat: 32.752546298028726, 
-  lng: 129.86865881602043
+  lat: 40.8296329213454,  
+  lng: 140.73392955858864
  }
   ]
   }
