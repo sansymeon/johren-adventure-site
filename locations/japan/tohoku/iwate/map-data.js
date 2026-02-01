38145bbd26cc6b7057e41e8d823250326477e563
sansymeon <stephenjnelson@gmail.com>
2026-02-01 15:54:42 +0900

    Update map-data.js

--- locations/japan/tohoku/iwate/map-data.js
+++ locations/japan/tohoku/iwate/map-data.js
@@ -11,6 +11,34 @@ window.MAP_CONFIG = {
         lat: 39.718873261561406,                 
         lng: 141.14334543732312
       },
+       {
+        id: "iwate_orthodox",
+        name: "盛ハリストス正教会・昇天聖堂",
+        nameEn: "Iwate Orthodox Church",
+        lat: 39.08736041329395,            
+        lng: 141.70685229327933
+      },
+      { 
+        id: "yamada_orthodox",
+        name: "山田ハリストス正教会・主の復活会堂",
+        nameEn: "Yamada Orthodox Church",
+        lat: 39.46703315417506,              
+        lng: 141.9510394814841
+      },
+      { 
+        id: "tonosei_orthodox",
+        name: "遠野正教会・聖太祖アウラアム・サッラ会堂",
+        nameEn: "Tonosei Orthodox Church",
+        lat: 39.34801785518456,              
+        lng: 141.56757262380552
+      },
+      { 
+        id: "ichinoseki_orthodox",
+        name: "一関ハリストス正教会・昇天聖堂",
+        nameEn: "Ichinoseki Orthodox Church",
+        lat: 38.92259105365958,             
+        lng: 141.1217309986392
+      },
         {
         id: "iwate_kawaguchi",
         name: "岩手川口駅",
