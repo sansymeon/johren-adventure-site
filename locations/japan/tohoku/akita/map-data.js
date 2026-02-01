da00d84debc1344ebd2a9b852087908eee3cbdc5
sansymeon <stephenjnelson@gmail.com>
2026-02-01 14:53:25 +0900

    Create map-data.js

---
+++ locations/japan/tohoku/akita/map-data.js
@@ -0,0 +1,24 @@
+window.MAP_CONFIG = {
+  iwate: {
+    center: [39.91842729730693, 141.19868947313708],
+    zoom: 10,
+
+    pins:  [
+  {
+        id: "morioka_orthodox",
+        name: "盛岡ハリストス正教会・聖十字架挙栄聖堂",
+        nameEn: "Morioka Orthodox Church",
+        lat: 39.718873261561406,                 
+        lng: 141.14334543732312
+      },
+        {
+        id: "iwate_kawaguchi",
+        name: "岩手川口駅",
+        nameEn: "Iwate Kawaguchi",
+        type: "station",
+        lat: 39.91886880943039,  
+        lng: 141.19933495479495
+ }
+  ]
+  }
+};
