503dfab53db2cc4cd346c79f23535287232edff3
sansymeon <stephenjnelson@gmail.com>
2026-02-02 16:08:48 +0900

    Update map-data.js

--- locations/japan/chubu/tokai/shizuoka/map-data.js
+++ locations/japan/chubu/tokai/shizuoka/map-data.js
@@ -1,69 +1,49 @@
 window.MAP_CONFIG = {
-  nagasaki: {
-    center: [32.752546298028726, 129.86865881602043],
+  shizuoka: {
+    center: [34.92260348311641, 138.07547619830308],
     zoom: 10,
 
     pins:  [
+      {
+        id: "kashiwakubo_orthodox",
+        name: "柏久保ハリストス正教会",
+        nameEn: "Kashiwakubo Orthodox Church",
+        type: "church",
+        lat: 34.97534355522436,      
+        lng: 138.9539535351832
+      },  
+      {
+        id: "hamamatsu_orthodox",
+        name: "浜松ハリストス正教会",
+        nameEn: "Hamamatsu Orthodox Church",
+        type: "church",
+        lat: 34.71300005366215,     
+        lng: 137.72066736769992
+      },  
   {
-        id: "peace_park",
-        name: "平和公園",
-        nameEn: "Peace Park Nagasaki",
-        type: "museum",
-        lat: 32.776992537763874,    
-        lng: 129.86393524060455
+        id: "shuzenji_orthodox",
+        name: "修善寺ハリストス正教会",
+        nameEn: "Shuzenji Orthodox Church",
+        type: "church",
+        lat: 34.97160178964159,     
+        lng: 138.93276886586673
       },  
         {
-        id: "urakami_cathedral",
-        name: "カトリック浦上教会（浦上天主堂）",
-        nameEn: "Urakami Cathedral",
+        id: "shizuoka_orthodox",
+        name: "静岡ハリストス正教会",
+        nameEn: "Shizuoka Orthodox Church",
         type: "church",
-        lat: 32.77611633657394,   
-        lng: 129.86835109327734
+        lat: 34.98061258220121,   
+        lng: 138.40191225237368
       }, 
       {
-  id: "unknown_shin_omura",
-  line: "unknown",
-  name: "新大村",
-  nameEn: "Shin-Omura",
+  id: "jr_tokaido_shizuoka",
+  line: "jr_tokaido",
+  name: "静岡",
+  nameEn: "Shizuoka",
   type: "station",
-  lat: 32.932626750179075,
-  lng: 129.95722857374489
-},
-{
-  id: "unknown_isahaya",
-  line: "unknown",
-  name: "諫早",
-  nameEn: "Isahaya",
-  type: "station",
-  lat: 32.85203767364156,
-  lng: 130.0412546541023
-},
-{
-  id: "unknown_aino",
-  line: "unknown",
-  name: "愛野",
-  nameEn: "Aino",
-  type: "station",
-  lat: 32.81895137075822,
-  lng: 130.15604553458053
-},
-{
-  id: "unknown_shimabara",
-  line: "unknown",
-  name: "島原",
-  type: "station",
-  nameEn: "Shimabara",
-  lat: 32.79027786498945,
-  lng: 130.370631075974
-},
-      {
-  id: "unknown_nagasaki",
-  line: "unknown",
-  name: "長崎",
-  nameEn: "Nagasaki",
-  type: "station",
-  lat: 32.752546298028726, 
-  lng: 129.86865881602043
+  lat: 34.97172644390538, 
+  lng: 138.38871819249098
  }
   ]
   }
