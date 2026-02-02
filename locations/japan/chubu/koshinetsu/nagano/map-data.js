864abe0d02f52c10e7c4b10b5b026c3c01480393
sansymeon <stephenjnelson@gmail.com>
2026-02-03 08:33:28 +0900

    Update map-data.js

--- locations/japan/chubu/koshinetsu/nagano/map-data.js
+++ locations/japan/chubu/koshinetsu/nagano/map-data.js
@@ -1,26 +1,26 @@
 window.MAP_CONFIG = {
-  tottori: {
-    center: [35.4788943796174, 134.05340613205206],
+  nagano: {
+    center: [36.17373433757445, 138.00848302174472],
     zoom: 10,
 
     pins:  [
   
         {
-        id: "tottori_catholic",
-        name: "カトリック鳥取教会",
-        nameEn: "Tottori Catholic Church",
+        id: "nagano_catholic",
+        name: "カトリック長野教会",
+        nameEn: "Nagano Catholic Church",
         type: "church",
-        lat: 35.504067595927765,   
-        lng: 134.23586392627794
+        lat: 36.65318529010604,    
+        lng: 138.19465798130796
       }, 
       {
-  id: "jr_west_tottori",
-  line: "jr_west",
-  name: "鳥取",
-  nameEn: "Tottori",
+  id: "jr_east_nagano",
+  line: "jr_east",
+  name: "長野",
+  nameEn: "Nagano",
   type: "station",
-  lat: 35.493999272656936,  
-  lng: 134.22585357939084
+  lat: 36.64322757398966,   
+  lng: 138.18878316966197
  }
   ]
   }
