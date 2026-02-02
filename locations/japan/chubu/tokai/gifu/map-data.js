9cd2221a6a70af7f57f8e21a160bc02d8f739b1a
sansymeon <stephenjnelson@gmail.com>
2026-02-03 08:46:34 +0900

    Update map-data.js

--- locations/japan/chubu/tokai/gifu/map-data.js
+++ locations/japan/chubu/tokai/gifu/map-data.js
@@ -1,26 +1,26 @@
 window.MAP_CONFIG = {
-  tottori: {
-    center: [35.4788943796174, 134.05340613205206],
+  gifu: {
+    center: [35.392503542089756, 136.80543142477384],
     zoom: 10,
 
     pins:  [
   
         {
-        id: "tottori_catholic",
-        name: "カトリック鳥取教会",
-        nameEn: "Tottori Catholic Church",
+        id: "gifu_catholic",
+        name: "カトリック岐阜教会",
+        nameEn: "Gifu Catholic Church",
         type: "church",
-        lat: 35.504067595927765,   
-        lng: 134.23586392627794
+        lat: 35.42512416965282,    
+        lng: 136.75071607080594
       }, 
       {
-  id: "jr_west_tottori",
-  line: "jr_west",
-  name: "鳥取",
-  nameEn: "Tottori",
+  id: "jr_tokaido_gifu",
+  line: "jr_tokaido",
+  name: "岐阜",
+  nameEn: "Gifu",
   type: "station",
-  lat: 35.493999272656936,  
-  lng: 134.22585357939084
+  lat: 35.409475306635464,   
+  lng: 136.75669090027228
  }
   ]
   }
