92f30cef894b63ad41de09e2efb3e294765d799e
sansymeon <stephenjnelson@gmail.com>
2026-02-02 21:27:13 +0900

    Update map-data.js

--- locations/japan/chugoku/shimane/map-data.js
+++ locations/japan/chugoku/shimane/map-data.js
@@ -6,57 +6,31 @@ window.MAP_CONFIG = {
     pins:  [
   
         {
-        id: "urakami_cathedral",
-        name: "カトリック浦上教会（浦上天主堂）",
-        nameEn: "Urakami Cathedral",
+        id: "matsue_catholic",
+        name: "松江カトリック教会",
+        nameEn: "Matsue Catholic Church",
         type: "church",
-        lat: 32.77611633657394,   
-        lng: 129.86835109327734
+        lat: 35.47399461420605,    
+        lng: 133.05542132569042
       }, 
       {
-  id: "unknown_shin_omura",
-  line: "unknown",
-  name: "新大村",
-  nameEn: "Shin-Omura",
-  type: "station",
-  lat: 32.932626750179075,
-  lng: 129.95722857374489
-},
 {
-  id: "unknown_isahaya",
-  line: "unknown",
-  name: "諫早",
-  nameEn: "Isahaya",
+  id: "jr_west_matsue",
+  line: "jr_west",
+  name: "松江",
   type: "station",
-  lat: 32.85203767364156,
-  lng: 130.0412546541023
-},
-{
-  id: "unknown_aino",
-  line: "unknown",
-  name: "愛野",
-  nameEn: "Aino",
-  type: "station",
-  lat: 32.81895137075822,
-  lng: 130.15604553458053
-},
-{
-  id: "unknown_shimabara",
-  line: "unknown",
-  name: "島原",
-  type: "station",
-  nameEn: "Shimabara",
-  lat: 32.79027786498945,
-  lng: 130.370631075974
+  nameEn: "Matsue",
+  lat: 35.464061700026434, 
+  lng: 133.06388299237187
 },
       {
-  id: "unknown_nagasaki",
-  line: "unknown",
-  name: "長崎",
-  nameEn: "Nagasaki",
+  id: "jr_west_hamada",
+  line: "jr_west",
+  name: "浜田",
+  nameEn: "Hamada",
   type: "station",
-  lat: 32.752546298028726, 
-  lng: 129.86865881602043
+  lat: 34.9025934187279,  
+  lng: 132.0888999545535
  }
   ]
   }
