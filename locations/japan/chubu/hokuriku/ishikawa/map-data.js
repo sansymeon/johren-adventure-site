f1fea6abcffac4ee9dda404beba4bbf46bfc8643
sansymeon <stephenjnelson@gmail.com>
2026-02-02 21:44:08 +0900

    Add files via upload

---
+++ locations/japan/chubu/hokuriku/ishikawa/map-data.js
@@ -0,0 +1,27 @@
+window.MAP_CONFIG = {
+  tottori: {
+    center: [35.4788943796174, 134.05340613205206],
+    zoom: 10,
+
+    pins:  [
+  
+        {
+        id: "tottori_catholic",
+        name: "カトリック鳥取教会",
+        nameEn: "Tottori Catholic Church",
+        type: "church",
+        lat: 35.504067595927765,   
+        lng: 134.23586392627794
+      }, 
+      {
+  id: "jr_west_tottori",
+  line: "jr_west",
+  name: "鳥取",
+  nameEn: "Tottori",
+  type: "station",
+  lat: 35.493999272656936,  
+  lng: 134.22585357939084
+ }
+  ]
+  }
+};
