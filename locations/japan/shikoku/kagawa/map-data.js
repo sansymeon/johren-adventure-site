85d5ce200831e80745602990290d6bb2d2cf0eaa
sansymeon <stephenjnelson@gmail.com>
2026-02-03 09:07:16 +0900

    Update map-data.js

--- locations/japan/shikoku/kagawa/map-data.js
+++ locations/japan/shikoku/kagawa/map-data.js
@@ -1,26 +1,26 @@
 window.MAP_CONFIG = {
-  tottori: {
-    center: [35.4788943796174, 134.05340613205206],
+  kagawa: {
+    center: [34.26623714078157, 133.93567327502433],
     zoom: 10,
 
     pins:  [
   
         {
-        id: "tottori_catholic",
-        name: "カトリック鳥取教会",
-        nameEn: "Tottori Catholic Church",
+        id: "takamatsu_catholic",
+        name: "カトリック桜町教会",
+        nameEn: "Cathedral of the Assumption Catholic Church",
         type: "church",
-        lat: 35.504067595927765,   
-        lng: 134.23586392627794
+        lat: 34.330734250742616,    
+        lng: 134.05148822244428
       }, 
       {
-  id: "jr_west_tottori",
-  line: "jr_west",
-  name: "鳥取",
-  nameEn: "Tottori",
+  id: "jr_shikoku_takamatsu",
+  line: "jr_shikoku",
+  name: "高松",
+  nameEn: "Takamatsu",
   type: "station",
-  lat: 35.493999272656936,  
-  lng: 134.22585357939084
+  lat: 34.35060753330822,   
+  lng: 134.0468136611
  }
   ]
   }
