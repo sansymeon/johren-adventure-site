bf3445c94c22e3c031e2dd246944225c7c053cc1
sansymeon <stephenjnelson@gmail.com>
2026-02-05 07:02:29 +0900

    Create map-data.js

---
+++ locations/japan/kyushu/fukuoka/fukuoka-north/map-data.js
@@ -0,0 +1,26 @@
+window.MAP_CONFIG = {
+  fukuoka_north: {
+    center: [31.589208084437992, 130.51962061457408],
+    zoom: 10,
+
+    pins:  [
+  {
+        id: "kokura_catholic",
+        name: "カトリック小倉教会",
+        nameEn: "Kokura Catholic Church",
+        type: "church",
+        lat: 33.8756788246324, 
+        lng: 130.88046499238203
+      },   
+      {
+  id: "jr_kyushu_kokura",
+  line: "jr_kyushu",
+  name: "小倉",
+  nameEn: "Kokura",
+  type: "station",
+  lat: 33.88672739937677, 
+  lng: 130.88246850628582
+ }
+  ]
+  }
+};
