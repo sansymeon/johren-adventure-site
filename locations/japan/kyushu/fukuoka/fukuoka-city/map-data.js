ae40eb3c127d3237ea279e3c78256eb57345033b
sansymeon <stephenjnelson@gmail.com>
2026-02-05 06:45:08 +0900

    Update map-data.js

--- locations/japan/kyushu/fukuoka/fukuoka-city/map-data.js
+++ locations/japan/kyushu/fukuoka/fukuoka-city/map-data.js
@@ -8,6 +8,7 @@ window.MAP_CONFIG = {
         id: "airport-tenjin",
         name: "天神",
         nameEn: "Tenjin",
+        type: "station",
         lat: 33.5913469503287,
         lng: 130.39891098113026
       },
@@ -15,6 +16,7 @@ window.MAP_CONFIG = {
         id: "airport-akasaka",
         name: "赤坂",
         nameEn: "Akasaka",
+        type: "station",
         lat: 33.589112760371975,
         lng: 130.3905781504464
       },
@@ -22,6 +24,7 @@ window.MAP_CONFIG = {
         id: "airport-ohorikoen",
         name: "大濠公園",
         nameEn: "Ohorikoen",
+        type: "station",
         lat: 33.59014125983869,
         lng: 130.37884327558552
       },
@@ -29,6 +32,7 @@ window.MAP_CONFIG = {
         id: "fukuoka-tenjin-minami",
         name: "天神南",
         nameEn: "Tenjin Minami",
+        type: "station",
         lat: 33.588670997721565,
         lng: 130.40265554434566
       },
@@ -36,32 +40,105 @@ window.MAP_CONFIG = {
         id: "fukuoka-yakuin",
         name: "薬院",
         nameEn: "Yakuin",
+        type: "station",
         lat: 33.5817638907711,
         lng: 130.40165000141508
       },
       {
+        id: "daimyo_catholic",
         name: "カトリック大名町教会",
         nameEn: "Daimyo Catholic Church",
+        type: "church",
         lat: 33.59028689597394,
         lng: 130.39490595063134
       },
       {
+        id: "fukuoka_art",
         name: "福岡市美術館",
         nameEn: "Fukuoka Art Museum",
+        type: "museum",
         lat: 33.58411652460625,
         lng: 130.37958344698214
       },
       {
+        id: "terumo_shrine",
         name: "光雲神社",
         nameEn: "Terumo Shrine",
+        type: "shrine",
         lat: 33.597145537533365,
         lng: 130.37643231206792
       },
       {
+        id: "gokoku_shrine",
         name: "福岡縣護國神社",
         nameEn: "Fukuoka Prefecture Gokoku Shrine",
+        type: "shrine",
         lat: 33.58171522775,
         lng: 130.38203472165
+      },
+        {
+      id: "chikuhi-shimoyamato", 
+      name: "下山門", 
+      nameEn: "Shimoyamato",  
+      type: "station",
+      lat: 33.581928964095525,
+      lng: 130.30821717928148 
+      },
+      { 
+        id: "chikuhi-meinohama", 
+        name: "姪浜", 
+        nameEn: "Meinohama",  
+         type: "station",
+         lat: 33.58355982784518,  
+        lng: 130.32484060626882 
+      },
+      { 
+        id: "airport-muromi", 
+        name: "室見", 
+        nameEn: "Muromi",  
+         type: "station",
+         lat: 33.58093333242043,  
+        lng: 130.34004725044602 
+      },
+      { 
+        id: "airport-fujisaki", 
+        name: "藤崎", 
+        nameEn: "Fujisaki",  
+         type: "station",
+         lat: 33.58128480587064,  
+        lng: 130.34811194647156 
+      },
+      { 
+        id: "airport-nishijin", 
+        name: "西新", 
+        nameEn: "Nishijin",  
+         type: "station",
+         lat: 33.58362772907049,  
+        lng: 130.35954911735814 
+      },
+      {
+        id: "meinohama_baptist", 
+        name: "姪浜バプテスト教会",
+        nameEn: "Meinohama Baptist Church",
+         type: "church",
+         lat: 33.584414790218574,
+        lng: 130.32510369901166
+      },
+      { 
+        id: "atago_shrine",
+        name: "鷲尾愛宕神社",
+        nameEn: "Atago Shrine", 
+         type: "shrine",
+         lat: 33.586443725202805,
+        lng: 130.33472168452056
+      },
+      { 
+        id: "junkoji_temple",
+        name: "順光寺",
+        nameEn: "Junkoji Temple", 
+         type: "temple",
+         lat: 33.59031331862292,
+        lng: 130.32182965079352
       }
   ]
   }
