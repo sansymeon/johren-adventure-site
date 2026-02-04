bc041b216baa89a4918d338e62e18e34f07fa67e
sansymeon <stephenjnelson@gmail.com>
2026-02-05 07:46:32 +0900

    Update map-data.js

--- locations/japan/kyushu/saga/saga-west/map-data.js
+++ locations/japan/kyushu/saga/saga-west/map-data.js
@@ -1,27 +1,139 @@
 window.MAP_CONFIG = {
-  tottori: {
-    center: [35.4788943796174, 134.05340613205206],
-    zoom: 10,
+  saga_west: {
+    center: [33.4533, 129.9782], 
+    zoom: 12,
 
-    pins:  [
-  
+ pins:  [
         {
-        id: "tottori_catholic",
-        name: "カトリック鳥取教会",
-        nameEn: "Tottori Catholic Church",
-        type: "church",
-        lat: 35.504067595927765,   
-        lng: 134.23586392627794
-      }, 
+      id: "jr-chikuhi-hamasaki", 
+      name: "浜崎", 
+      nameEn: "Hamasaki", 
+      type: "station",
+      lat: 33.44672, 
+      lng: 130.036798 
+      },
+      { 
+        id: "jr-chikuhi-nijinomatsubara", 
+       name: "虹の松原", 
+       nameEn: "Nijinomatsubara", 
+        type: "station",
+        lat: 33.441033, 
+        lng: 130.01619 
+      },
+      { 
+        id: "jr-chikuhi-higashi-karatsu", 
+       name: "東唐津", 
+       nameEn: "Higashi Karatsu", 
+         type: "station",
+        lat: 33.439393, 
+        lng: 129.996837 
+  
+      },
+    
+      { 
+        id: "jr-chikuhi-watada", 
+       name: "和多田", 
+        nameEn: "Watada", 
+         type: "station",
+        lat: 33.436933, 
+        lng: 129.980768 
+      },
+      { 
+        id: "jr-chikuhi-karatsu", 
+       name: "唐津", 
+       nameEn: "Karatsu", 
+        type: "station",
+        lat: 33.446223, 
+       lng: 129.967587 
+      },
+      { 
+        id: "jr-chikuhi-nishi-karatsu", 
+       name: "西唐津", 
+       nameEn: "Nishi Karatsu", 
+         type: "station",
+        lat: 33.461434, 
+        lng: 129.95748 
+      },
+      { 
+        id: "jr-karatsu-yamamoto", 
+       name: "山本", 
+       nameEn: "Yamamoto", 
+        type: "station",
+        lat: 33.39278208271683, 
+        lng: 129.98118155167737 
+      },
+      { 
+        id: "jr-karatsu-ouchi", 
+       name: "相知", 
+        nameEn: "Ouchi", 
+        type: "station",
+        lat: 33.34699039849446, 
+        lng: 130.01952634118035 
+      },
+      { 
+        id: "jr-karatsu-iwaya", 
+       name: "岩屋", 
+        nameEn: "Iwaya", 
+        type: "station",
+        lat: 33.33076505677805, 
+        lng: 130.03827683163834 
+      },
       {
-  id: "jr_west_tottori",
-  line: "jr_west",
-  name: "鳥取",
-  nameEn: "Tottori",
-  type: "station",
-  lat: 35.493999272656936,  
-  lng: 134.22585357939084
- }
+        id: "karatsu_catholic",
+        name: "カトリック唐津教会",      
+        nameEn: "Karatsu Catholic Church",
+         type: "church",
+        lat: 33.45252801155086,            
+        lng: 129.9614152412968
+      },
+    { 
+         id: "hikiyama_hall",
+       name: "曳山展示場", 
+       nameEn: "Hikiyama Exhibition Hall", 
+        type: "museum",
+       lat: 33.445946414612074, 
+       lng: 129.96991124129468 
+      },
+      { 
+        id: "karatsu_castle",
+        name: "唐津城", 
+       nameEn: "Karatsu Castle", 
+        type: "museum",
+          lat: 33.45345765716856, 
+       lng: 129.9782080418245 
+      },
+      { 
+        id: "takatori_museum",
+        name: "旧高取邸", 
+       nameEn: "Former Takatori Residence", 
+        type: "museum",
+          lat: 33.45437629769318, 
+       lng: 129.97209266442803 
+      },
+      { 
+        id: "karatsu_shrine",
+        name: "唐津神社", 
+       nameEn: "Karatsu Shrine", 
+        type: "shrine",
+      lat: 33.45227410257489, 
+       lng: 129.96948404957564 
+      },
+      { 
+        id: "myoken_shrine",
+        name: "妙見神社", 
+       nameEn: "Myoken Shrine", 
+         type: "shrine",
+          lat: 33.46854559781762, 
+       lng: 129.95317852240166 
+      },
+      { 
+        id: "gonshoji_temple",
+        name: "近松寺", 
+       nameEn: "Gonshoji Temple", 
+         type: "temple",
+      lat: 33.449901908390125, 
+       lng: 129.96520797775324 
+      }
   ]
   }
 };
