ee404f0401dbfce2f3e715d8aed704a4e0905c27
sansymeon <stephenjnelson@gmail.com>
2026-02-07 21:14:51 +0900

    Update map-data.js

--- data/japan/map-data.js
+++ data/japan/map-data.js
@@ -1,10 +1,21 @@
-window.MAP_CONFIG = {
-  itoshima: {
-    center: [33.557, 130.199],
-    zoom: 12,
-    bounds: [
-      [33.48, 130.05],   // southwest
-      [33.65, 130.28]    // northeast
-    ]
-  }
-};data/japan/map-data.js
+window.JAPAN_MAP_DATA = {
+  pins: [
+    {
+      id: "ito_art_museum",
+      name: "伊都郷土美術館",
+      nameEn: "Ito Art Museum",
+      type: "museum",
+      lat: 33.56063278048929,
+      lng: 130.20447283810256
+    },
+    {
+      id: "chikuhi_chikuzen_maebaru",
+      name: "筑前前原",
+      nameEn: "Chikuzen Maebaru",
+      type: "station",
+      lat: 33.557082,
+      lng: 130.199305
+    }
+    // ...every other Japan pin
+  ]
+};
