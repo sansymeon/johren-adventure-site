af3ba89a31e8e82021f0e2c871b4bc9749f59338
sansymeon <stephenjnelson@gmail.com>
2026-02-07 21:12:50 +0900

    Update map-data.js

--- data/japan/map-data.js
+++ data/japan/map-data.js
@@ -1,1 +1,10 @@
-data/japan/map-data.js
+window.MAP_CONFIG = {
+  itoshima: {
+    center: [33.557, 130.199],
+    zoom: 12,
+    bounds: [
+      [33.48, 130.05],   // southwest
+      [33.65, 130.28]    // northeast
+    ]
+  }
+};data/japan/map-data.js
