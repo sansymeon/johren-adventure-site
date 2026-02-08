b95e8b52f60f1a780f9565c1707181ff28bf5c75
sansymeon <stephenjnelson@gmail.com>
2026-02-08 21:43:38 +0900

    Update map.js

--- js/map/map.js
+++ js/map/map.js
@@ -28,9 +28,10 @@
     map.setMaxBounds(bounds);
   }
 
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: ""
-  }).addTo(map);
+ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
 
   // --------------------------------
   // Icons (extend freely)
