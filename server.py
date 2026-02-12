9d1c2ce5e392a7cb8e133e5a0e3adeb9ef584904
sansymeon <stephenjnelson@gmail.com>
2026-02-12 10:32:58 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
-.txt
+import http.server
+import socketserver
+import os
+
+PORT = 8080
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    pass
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
+
